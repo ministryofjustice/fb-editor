3abcb14da64d3b9d34397a872616a1a217683a2c
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-02-09 09:56:49 -0300

    Save a thumbnail for page flow

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,20 @@
 module ApplicationHelper
   include MetadataPresenter::ApplicationHelper
+
+  def display_thumbnail(page:, service:)
+    html = MetadataPresenter::PagesController.render(
+      page.template,
+      assigns: { page: page, service: service, user_data: {} })
+
+    absolute_html = Grover::HTMLPreprocessor.process html,
+      'http://localhost:3001/', 'http'
+
+    image_path = Rails.root.join('public', "#{page.uuid}.png").to_s
+
+    if File.exist?(image_path)
+      %{<img src='/#{page.uuid}.png' alt='thumbnail' />}.html_safe
+    else
+      Grover.new(absolute_html).to_png(image_path)
+    end
+  end
 end
