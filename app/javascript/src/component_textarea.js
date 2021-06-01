5ece9de0cf41b1f25fb810c141f1dbb931a7c0c5
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2021-06-01 17:27:42 +0100

    Convert Textarea component to it's own Class for better isolation/organisation

---
+++ app/javascript/src/component_textarea.js
@@ -0,0 +1,37 @@
+/**
+ * Date Question
+ * ----------------------------------------------------
+ * Description:
+ * Textarea component extension of Question
+ *
+ * Documentation:
+ *
+ *     - Requires jQuery & jQueryUI
+ *       https://api.jquery.com/
+ *       https://api.jqueryui.com/
+ *
+ *     - TODO:
+ *       (steven.burnell@digital.justice.gov.uk to add).
+ *
+ **/
+
+
+const utilities = require('./utilities');
+const mergeObjects = utilities.mergeObjects;
+const Question = require('./question');
+
+const SELECTOR_HINT = ".govuk-hint";
+const SELECTOR_LABEL = "label h1, label h2, legend h1, legend h2";
+
+class TextareaComponent extends Question {
+  constructor($node, config) {
+    super($node, mergeObjects({
+      // Add stuff here if you want to set defaults
+      selectorLabel: SELECTOR_LABEL,
+      selectorHint: SELECTOR_HINT
+    }, config));
+  }
+}
+
+
+module.exports = TextareaComponent;
