8c0b8855aa9a3c78d5881392cda59b7ed665218a
brenetic <brendan.butler@digital.justice.gov.uk>
2021-06-16 15:25:14 +0100

    Remove testable editors from Cloud Platform
    This adds the ability to remove any testable editors whose branches have
    been merged into main and then deleted from Github.
    
    Currently this is a rake task but we will add a CircleCI job to trigger
    the automatic removal overnight.

---
+++ app/services/testable_editor_remover.rb
@@ -0,0 +1,73 @@
+class TestableEditorRemover
+  WEB = '-web-test'.freeze
+  WORKERS = '-workers-test'.freeze
+  CONFIGURATIONS = [
+    { type: 'configmap', append: '-config-map' },
+    { type: 'deployment', append: WEB },
+    { type: 'deployment', append: WORKERS },
+    { type: 'ingress', append: '-ing-test' },
+    { type: 'service', append: '-svc-test' },
+    { type: 'servicemonitor', append: '-service-monitor-test' }
+  ].freeze
+  NAMESPACE = 'formbuilder-saas-test'.freeze
+  ORIGIN = 'origin/'.freeze
+  TESTABLE = 'testable-'.freeze
+
+  def call
+    testable_editors_to_remove.each do |testable_editor|
+      k8s_config_removal_service(testable_editor).tap do |removal_service|
+        removal_service.call
+        NotificationService.notify(removal_service.status.join("\n"))
+      end
+    end
+  end
+
+  private
+
+  def testable_editors_to_remove
+    unique_testable_deployments - testable_editor_branches
+  end
+
+  def testable_editor_branches
+    branches = remote_branches.split(' ').map do |branch|
+      branch.gsub(ORIGIN, '') if branch.include?(TESTABLE)
+    end
+    branches.compact
+  end
+
+  def remote_branches
+    run('$(which git) branch -r')
+  end
+
+  def unique_testable_deployments
+    deployments = kubernetes_deployments.split("\n").map do |deployment|
+      if deployment.include?(TESTABLE)
+        deployment.split(' ')[0].gsub(/#{WEB}|#{WORKERS}/, '')
+      end
+    end
+    deployments.compact.uniq
+  end
+
+  def kubernetes_deployments
+    run("$(which kubectl) get deployments -n #{NAMESPACE}")
+  end
+
+  def run(cmd)
+    Publisher::Utils::Shell.output_of(cmd)
+  rescue Publisher::Utils::Shell::CmdFailedError => e
+    Sentry.capture_exception(e)
+  end
+
+  def k8s_config_removal_service(testable_editor)
+    K8sConfigRemovalService.new(namespace: NAMESPACE, targets: targets(testable_editor))
+  end
+
+  def targets(testable_editor)
+    CONFIGURATIONS.map do |configuration|
+      {
+        config: configuration[:type],
+        name: "#{testable_editor}#{configuration[:append]}"
+      }
+    end
+  end
+end
---
+++ spec/services/k8s_config_removal_service_spec.rb
@@ -0,0 +1,73 @@
+RSpec.describe K8sConfigRemovalService do
+  subject(:removal_service) do
+    described_class.new(namespace: namespace, targets: targets)
+  end
+
+  context 'removing target kubernetes configurations' do
+    let(:namespace) { 'it is full of stars' }
+    let(:targets) do
+      [
+        { config: 'config1', name: 'something-to-remove' },
+        { config: 'config2', name: 'something-else-to-remove' },
+        { config: 'config3', name: 'this-too' }
+      ]
+    end
+
+    context 'successfully removing configurations' do
+      let(:status_messages) do
+        [
+          'Successfully deleted config1 for something-to-remove',
+          'Successfully deleted config2 for something-else-to-remove',
+          'Successfully deleted config3 for this-too'
+        ]
+      end
+
+      before do
+        allow(Publisher::Utils::Shell).to receive(:output_of).and_return(true)
+      end
+
+      it 'creates the correct success status messages' do
+        removal_service.call
+        expect(removal_service.status).to eq(status_messages)
+      end
+    end
+
+    context 'failed to remove configurations' do
+      let(:status_messages) do
+        [
+          'Failed to delete config1 for something-to-remove',
+          'Failed to delete config2 for something-else-to-remove',
+          'Failed to delete config3 for this-too'
+        ]
+      end
+
+      before do
+        allow(Publisher::Utils::Shell).to receive(
+          :output_of
+        ).and_raise(Publisher::Utils::Shell::CmdFailedError)
+      end
+
+      it 'creates the correct failure status messages' do
+        removal_service.call
+        expect(removal_service.status).to eq(status_messages)
+      end
+    end
+
+    context 'kubectl commands' do
+      let(:config) { 'some-config' }
+      let(:name) { 'some-name' }
+      let(:targets) { [{ config: config, name: name }] }
+
+      before do
+        allow(Publisher::Utils::Shell).to receive(:capture_with_stdin).and_return('success')
+      end
+
+      it 'runs the correct kubectil commands' do
+        expect(Publisher::Utils::Shell).to receive(
+          :output_of
+        ).with('$(which kubectl)', "delete #{config} #{name} -n #{namespace}").once
+        removal_service.call
+      end
+    end
+  end
+end
