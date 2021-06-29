fc553374c5bc6c4fae6df7049439ff23f2507f45
brenetic <brendan.butler@digital.justice.gov.uk>
2021-06-29 14:46:46 +0100

    Check for optional text on pages

--- acceptance/support/common_steps.rb
+++ acceptance/support/common_steps.rb
@@ -1,4 +1,11 @@
 module CommonSteps
+  OPTIONAL_TEXT = [
+    '[Optional section heading]',
+    '[Optional lede paragraph]',
+    '[Optional content]',
+    '[Optional hint text]'
+  ]
+
   def given_I_am_logged_in
     editor.load
     editor.sign_in_button.click
@@ -160,6 +167,7 @@ module CommonSteps
     within_window(preview_page) do
       expect(page.find('input[type="submit"]')).to_not be_disabled
       expect(page.text).to include('Question')
+      then_I_should_not_see_optional_text(page.text)
       yield if block_given?
     end
   end
@@ -229,5 +237,9 @@ module CommonSteps
   rescue Capybara::ElementNotFound
     # body elements
     element.find('.output', visible: false)
+  end
+
+  def then_I_should_not_see_optional_text(text)
+    OPTIONAL_TEXT.each { |optional| expect(text).not_to include(optional) }
   end
 end
