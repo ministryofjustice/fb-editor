f16a863c2e2577d672519e9e7430c7af9ff91709
brenetic <brendan.butler@digital.justice.gov.uk>
2021-06-29 15:47:40 +0100

    Add acceptance test for optional checkboxes in preview

--- acceptance/support/common_steps.rb
+++ acceptance/support/common_steps.rb
@@ -180,6 +180,21 @@ module CommonSteps
     editor.save_page_button.click
   end
 
+  def when_I_want_to_select_question_properties
+    editor.question_heading.first.click
+    editor.question_three_dots_button.click
+    editor.should_not have_css('span', text: 'Delete...')
+  end
+
+  def and_I_want_to_set_a_question_optional
+    editor.required_question.click
+  end
+
+  def and_I_update_the_question_to_be_optional
+    editor.choose 'No', visible: false
+    editor.click_button 'Update'
+  end
+
   def given_I_have_a_single_question_page_with_radio
     given_I_add_a_single_question_page_with_radio
     and_I_add_a_page_url
