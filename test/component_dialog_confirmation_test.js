dab000b9d319525d18ecfc1a398d9c33b2e4f031
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2021-06-30 09:35:38 +0100

    Initial commit of basic framework

---
+++ test/component_dialog_confirmation_test.js
@@ -0,0 +1,54 @@
+require('./setup');
+const expect = require('chai').expect;
+
+describe("Confirmation Dialog", function() {
+
+  const DialogConfirmation = require('../app/javascript/src/component_dialog_confirmation');
+  const CANCEL_TEXT = "Dialog says Cancel";
+  const OK_TEXT = "Dialog says Ok";
+  const ACTIVATOR_CLASSES = "activator-classname and-some-activator";
+  const BUTTON_CLASSES = "button-classname and-some-button";
+  const DIALOG_CLASSES = "dialog-classname and-some-dialog";
+  const DIALOG_ID = "component-dialog-confirmation-test-id";
+  const CONTAINER_ID = "component-dialog-confirmation-test-container";
+  var dialog;
+
+  before(function() {
+    // jQuyery is present in document because the
+    // components use it, so we can use it here.
+
+    var $container = $("<div></div>");
+    var $dialog = $(`<div class="component component-dialog-confirmation">
+          <h3 data-node="heading" class="heading">General heading here</h3>
+          <p data-node="content">General message here</p>
+        </div>`);
+
+    $dialog.attr("id", DIALOG_ID);
+    $container.attr("id", CONTAINER_ID);
+    $container.append($dialog);
+    $(document.body).append($container);
+
+    dialog = new DialogConfirmation($dialog, {
+      autoOpen: false,
+      cancelText: CANCEL_TEXT,
+      okText: OK_TEXT,
+      classes: {
+        "ui-activator": ACTIVATOR_CLASSES,
+        "ui-button": BUTTON_CLASSES,
+        "ui-dialog": DIALOG_CLASSES
+      }
+    });
+
+  });
+
+  after(function() {
+    $("#" + DIALOG_ID).dialog("destroy");
+    $("#" + CONTAINER_ID).remove();
+  });
+
+  describe("Ready", function() {
+    it("should pass here", function() {
+      expect(1).to.equal(1);
+    });
+  });
+});
