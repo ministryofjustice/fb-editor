ba3fc2cd8af6b3420e596b9973ce29244064dd18
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2021-07-27 13:42:40 +0100

    Add toggle for dev-only colours

--- app/javascript/src/controller_branches.js
+++ app/javascript/src/controller_branches.js
@@ -28,6 +28,8 @@ class BranchesController extends DefaultController {
       case "new":
         BranchesController.create.call(this);
     }
+
+    addMattsButton(); // Dev only while branches is WIP
   }
 }
 
@@ -37,5 +39,47 @@ class BranchesController extends DefaultController {
 BranchesController.create = function() {
   console.log("BranchesController.create is alive");
 }
+
+
+/* TO BE REMOVED
+ * Only here during early stages of development
+ * for developer aid and humour value :-)
+ **/
+function addMattsButton() {
+  var $mattsButton = $("<button>Matt's button</button>");
+  $mattsButton.css({
+    "animation": "blinker 0.5s linear infinite",
+    "background-color": "red",
+    "color": "white",
+    "display": "none",
+    "font-size": "30px",
+    "font-weight": "bold",
+    "padding": "10px",
+    "position": "absolute",
+    "right": "50px",
+    "top": "200px"
+  });
+
+  $(document.body).append($mattsButton);
+  $("#form-navigation-heading").on("click", function(event) {
+    $mattsButton.toggle();
+  });
+
+  if(document.cookie.search("colours=on") == 0) {
+    $(document.body).addClass("dev-colours-on");
+  }
+
+  $mattsButton.on("click", function() {
+    if(document.cookie.search("colours=on") == 0) {
+      document.cookie = "colours=off";
+      $(document.body).removeClass("dev-colours-on");
+    }
+    else {
+      document.cookie = "colours=on";
+      $(document.body).addClass("dev-colours-on");
+    }
+  });
+}
+
 
 module.exports = BranchesController;
