a3963711a337dda503c992eebba68a9b56085c90
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-07-29 07:32:53 -0300

    Change api spec to test the most important thing
    We only care about conditionals index name attribute on the api endpoint
    and not
    with the html tags generated
    
    Co-authored-by: Steven Burnell <steven.burnell@digital.justice.gov.uk>

--- spec/requests/api/branches_spec.rb
+++ spec/requests/api/branches_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe 'Branch' do
 
       it 'returns the next conditional' do
         expect(response.body).to include(
-          'select name="branch[conditionals_attributes][2][next]"'
+          'name="branch[conditionals_attributes][2][next]"'
         )
       end
     end
@@ -30,7 +30,7 @@ RSpec.describe 'Branch' do
 
       it 'returns the next conditional' do
         expect(response.body).to include(
-          'select name="branch[conditionals_attributes][3][next]"'
+          'name="branch[conditionals_attributes][3][next]"'
         )
       end
     end
