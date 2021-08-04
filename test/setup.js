948c5a898c4814872eec0a1aa018bccf09403364
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2021-08-04 19:44:57 +0100

    Disable focus in test environment to stop errors (due to lack of support)

--- test/setup.js
+++ test/setup.js
@@ -10,17 +10,26 @@ const dom = new JSDOM(`<html>
     </body>
   </html>`);
 
-  global.window = dom.window;
-  global.document = window.document;
-  global.jQuery = require( 'jquery' )( window );
-  global.$ = jQuery;
-  require('jquery-ui/ui/widget');
-  require('jquery-ui/ui/unique-id');
-  require('jquery-ui/ui/widgets/button');
-  require('jquery-ui/ui/widgets/dialog');
-  require('jquery-ui/ui/widgets/menu');
-  require('jquery-ui/ui/safe-active-element');
-  require('jquery-ui/ui/data');
-  require('jquery-ui/ui/tabbable');
-  require('jquery-ui/ui/focusable');
-  require('jquery-ui/ui/safe-blur');
+// Setup environment
+global.window = dom.window;
+global.document = window.document;
+
+// Setup jQuery and jQueryUI requirements
+global.jQuery = require( 'jquery' )( window );
+global.$ = jQuery;
+require('jquery-ui/ui/widget');
+require('jquery-ui/ui/unique-id');
+require('jquery-ui/ui/widgets/button');
+require('jquery-ui/ui/widgets/dialog');
+require('jquery-ui/ui/widgets/menu');
+require('jquery-ui/ui/safe-active-element');
+require('jquery-ui/ui/data');
+require('jquery-ui/ui/tabbable');
+require('jquery-ui/ui/focusable');
+require('jquery-ui/ui/safe-blur');
+
+// Disable stuff not support by test environment
+//
+global.$.fn.focus = function() {
+  // Do nothing because it throws an error due to environment not supporting focus.
+}
