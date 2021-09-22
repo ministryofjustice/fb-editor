741dca60281e36906f9afd255b566f6e29e546bf
brenetic <brendan.butler@digital.justice.gov.uk>
2021-09-22 17:42:21 +0100

    Refactor route and traversable
    Change the Route object and Traversable. The Route object will now pick
    the first conditional destination and follow that through until the end
    of the service flow.
    
    Tweak Traversable to only return page UUIDs.
    
    [This PR](https://github.com/ministryofjustice/fb-metadata-presenter/pull/179)
    explains the logic change to the Route model better.

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,33 +1,49 @@
 class Route
-  attr_reader :service, :traverse_from
-  attr_accessor :page_uuids, :routes
+  attr_reader :traverse_from
+  attr_accessor :flow_uuids, :routes, :column
 
-  def initialize(service:, traverse_from:)
+  def initialize(service:, traverse_from:, column: 0)
     @service = service
     @traverse_from = traverse_from
+    @column = column
     @routes = []
-    @page_uuids = []
+    @flow_uuids = []
   end
 
   def traverse
     @flow_uuid = traverse_from
 
+    index = column
     until @flow_uuid.blank?
+      if index > service.flow.size
+        Sentry.capture_message('Exceeded total number of flow objects')
+        break
+      end
+
+      @flow_uuids.push(@flow_uuid) unless @flow_uuids.include?(@flow_uuid)
       flow_object = service.flow_object(@flow_uuid)
 
       if flow_object.branch?
-        destination_uuids(flow_object).each do |uuid|
-          @routes.push(Route.new(service: service, traverse_from: uuid))
+        destinations = destination_uuids(flow_object)
+        @flow_uuid = destinations.shift
+
+        column_number = index + 1 # column after the branching object
+        destinations.each do |uuid|
+          @routes.push(Route.new(service: service, traverse_from: uuid, column: column_number))
         end
-        break
       else
-        @page_uuids.push(@flow_uuid) unless @page_uuids.include?(@flow_uuid)
         @flow_uuid = flow_object.default_next
       end
+
+      index += 1
     end
+
+    @flow_uuids
   end
 
   private
+
+  attr_reader :service
 
   def destination_uuids(flow_object)
     flow_object.conditionals.map(&:next).push(flow_object.default_next)
