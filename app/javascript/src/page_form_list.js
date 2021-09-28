7e52647190c09c73fb1851e9625cde96a6330901
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2021-09-28 11:43:07 +0100

    Update to use new ActivatedFormDialog component for better code sharing

--- app/javascript/src/page_form_list.js
+++ app/javascript/src/page_form_list.js
@@ -16,7 +16,7 @@
  **/
 
 
-const ActivatedDialog = require('./component_activated_dialog');
+const ActivatedFormDialog = require('./component_activated_form_dialog');
 const DefaultController = require('./controller_default');
 
 
@@ -25,50 +25,26 @@ class FormListPage extends DefaultController {
     super(app);
 
     // Create dialog for handling new form input and error reporting.
-    new FormCreateDialog(this, $("[data-component='FormCreateDialog']"));
-
+    new CreateFormDialog($("[data-component='FormCreateDialog']"));
   }
 }
 
 
-/* Wrap the create form form in a dialog effect.
- * Errors will also show here on page return.
- **/
-class FormCreateDialog {
-  constructor(page, $node, config) {
-    var formCreateDialog = this;
-    var $form = $node.find("form");
-    var $submit = $form.find(":submit");
-    var $errors = $node.find(".govuk-error-message");
-
-    new ActivatedDialog($node, {
+class CreateFormDialog {
+  constructor($node) {
+    var errorMessageSelector = ".govuk-error-message";
+    var $errors = $node.find(errorMessageSelector);
+    new ActivatedFormDialog($("[data-component='FormCreateDialog']"), {
       autoOpen: $errors.length ? true: false,
       cancelText: $node.data("cancel-text"),
-      okText: $submit.val(),
       activatorText: $node.data("activator-text"),
+      selectorErrors: errorMessageSelector,
+      removeErrorClasses: ".govuk-form-group--error",
       classes: {
         "ui-button": "govuk-button",
         "ui-activator": "govuk-button fb-govuk-button"
-      },
-      onOk: () => {
-        formCreateDialog.$form.submit();
-      },
-      onClose: () => {
-        formCreateDialog.clearErrors();
       }
     });
-
-    // Disable button as we're replacing it.
-    $submit.attr("disabled", true);
-
-    this.$form = $form;
-    this.$submit = $submit;
-    this.$errors = $errors;
-  }
-
-  clearErrors() {
-    this.$errors.parents().removeClass("govuk-form-group--error");
-    this.$errors.remove();
   }
 }
 
