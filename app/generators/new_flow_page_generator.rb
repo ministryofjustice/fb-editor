108dd2f950ef19b8e5f2138e7fbb57a825e8ca86
brenetic <brendan.butler@digital.justice.gov.uk>
2021-10-04 14:33:37 +0100

    Correctly set flow object default next when adding a new page
    When inserting a page the flow object that represents that page needs to
    have its default next point to what the previous object to it was
    pointing too.
    
    This has not been a problem until now because we rely on the order of
    the pages when presenting the service flow.

--- app/generators/new_flow_page_generator.rb
+++ app/generators/new_flow_page_generator.rb
@@ -1,7 +1,7 @@
 class NewFlowPageGenerator
-  def initialize(page_uuid:, page_index:, latest_metadata:)
+  def initialize(page_uuid:, latest_metadata:, default_next: nil)
     @page_uuid = page_uuid
-    @page_index = page_index
+    @default_next = default_next
     @latest_metadata = latest_metadata
   end
 
@@ -11,24 +11,12 @@ class NewFlowPageGenerator
 
   private
 
-  attr_reader :page_uuid, :page_index, :latest_metadata
+  attr_reader :page_uuid, :default_next, :latest_metadata
 
   def flow_page_metadata
     flow_page = default_metadata
-    flow_page['next']['default'] = default_next
+    flow_page['next']['default'] = default_next.to_s
     flow_page
-  end
-
-  def default_next
-    next_page.present? ? next_page['_uuid'] : ''
-  end
-
-  def next_page
-    @next_page ||= begin
-      return if page_index.nil?
-
-      latest_metadata['pages'][page_index + 1]
-    end
   end
 
   def default_metadata
