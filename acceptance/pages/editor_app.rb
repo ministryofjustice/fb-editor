478189c91481e0a5da5634ce1aaf8b53a116f09b
Natalie Seeto <njseeto@users.noreply.github.com>
2021-10-26 10:30:57 +0100

    Have correct partial path
    The path defaults to 'pages/_flow_page_menu'

--- acceptance/pages/editor_app.rb
+++ acceptance/pages/editor_app.rb
@@ -109,7 +109,7 @@ class EditorApp < SitePrism::Page
   data_content_id :first_extra_component, 'page[extra_components[0]]'
 
   elements :form_pages, '.flow-item'
-  elements :form_urls, '.form-step a.govuk-link'
+  elements :form_urls, '.flow-item a.govuk-link'
   elements :preview_page_images, '.flow-item img.body'
   element :three_dots_button, '.flow-menu-activator'
   element :preview_page_link, :link, I18n.t('actions.preview_page')
