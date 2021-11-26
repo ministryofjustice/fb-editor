08419acd49f35a16169801a16605f737e9dbf4f6
Natalie Seeto <njseeto@users.noreply.github.com>
2021-11-26 16:59:58 +0000

    Add publishing warning when CYA and confirmation pages are not in a form

--- acceptance/features/publishing_page_spec.rb
+++ acceptance/features/publishing_page_spec.rb
@@ -4,15 +4,80 @@ feature 'Branching errors' do
   let(:editor) { EditorApp.new }
   let(:service_name) { generate_service_name }
   let(:page_url) { 'palpatine' }
+  let(:warning_both) { I18n.t('publish.warning.both_pages') }
+  let(:warning_cya) { I18n.t('publish.warning.cya') }
+  let(:warning_confirmation) { I18n.t('publish.warning.confirmation') }
 
   background do
     given_I_am_logged_in
     given_I_have_a_service
   end
 
-  scenario 'when visiting the publishing page' do
+  scenario 'when visiting the publishing page with submitting pages present' do
+    given_I_have_a_single_question_page_with_upload
+    and_I_return_to_flow_page
+
+    # when we add the new default template,
+    # adding the cya and confirmation pages will not be possible
+    # and this test should fail
+    given_I_add_a_check_answers_page
+    and_I_add_a_page_url('cya')
+    when_I_add_the_page
+    and_I_return_to_flow_page
+
+    given_I_add_a_confirmation_page
+    and_I_add_a_page_url('confirmation')
+    when_I_add_the_page
+
+    and_I_return_to_flow_page
     when_I_visit_the_publishing_page
     then_I_should_be_on_the_publishing_page
+    then_I_should_not_see_warning_both_text
+    then_I_should_not_see_warning_cya_text
+    then_I_should_not_see_warning_confirmation_text
+  end
+
+  # when we add the new default template, the cya and confirmation pages will present
+  # and the next 3 tests should fail
+  scenario 'when visiting the publishing page without submitting pages present' do
+    given_I_have_a_single_question_page_with_upload
+    and_I_return_to_flow_page
+    when_I_visit_the_publishing_page
+    then_I_should_be_on_the_publishing_page
+    then_I_should_see_warning_both_text
+    then_I_should_not_see_warning_cya_text
+    then_I_should_not_see_warning_confirmation_text
+  end
+
+  scenario 'when visiting the publishing page without cya page present' do
+    given_I_have_a_single_question_page_with_upload
+    and_I_return_to_flow_page
+
+    given_I_add_a_confirmation_page
+    and_I_add_a_page_url('confirmation')
+    when_I_add_the_page
+
+    when_I_visit_the_publishing_page
+    then_I_should_be_on_the_publishing_page
+    then_I_should_not_see_warning_both_text
+    then_I_should_not_see_warning_confirmation_text
+    then_I_should_see_warning_cya_text
+  end
+
+  scenario 'when visiting the publishing page without confirmation page present' do
+    given_I_have_a_single_question_page_with_upload
+    and_I_return_to_flow_page
+
+    given_I_add_a_check_answers_page
+    and_I_add_a_page_url('cya')
+    when_I_add_the_page
+    and_I_return_to_flow_page
+
+    when_I_visit_the_publishing_page
+    then_I_should_be_on_the_publishing_page
+    then_I_should_not_see_warning_both_text
+    then_I_should_not_see_warning_cya_text
+    then_I_should_see_warning_confirmation_text
   end
 
   def when_I_visit_the_publishing_page
@@ -24,5 +89,29 @@ feature 'Branching errors' do
     buttons_text = page.all(:css, 'input.fb-govuk-button', visible: false).map(&:value)
     expect(buttons_text).to include(I18n.t('publish.test.button'))
     expect(buttons_text).to include(I18n.t('publish.live.button'))
+  end
+
+  def then_I_should_not_see_warning_both_text
+    expect(editor.text).to_not include(warning_both)
+  end
+
+  def then_I_should_not_see_warning_cya_text
+    expect(editor.text).to_not include(warning_cya)
+  end
+
+  def then_I_should_not_see_warning_confirmation_text
+    expect(editor.text).to_not include(warning_confirmation)
+  end
+
+  def then_I_should_see_warning_both_text
+    expect(editor.text).to include(warning_both)
+  end
+
+  def then_I_should_see_warning_cya_text
+    expect(editor.text).to include(warning_cya)
+  end
+
+  def then_I_should_see_warning_confirmation_text
+    expect(editor.text).to include(warning_confirmation)
   end
 end
