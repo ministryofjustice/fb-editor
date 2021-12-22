a02e291713678b09a92a1283a424b12313ffc13e
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2021-12-22 17:05:52 +0000

    Add DownForwardUpPath interaction with latest Line class

--- app/javascript/src/component_flow_connector_path.js
+++ app/javascript/src/component_flow_connector_path.js
@@ -557,12 +557,56 @@ class DownForwardUpPath extends FlowConnectorPath {
   }
 
   set path(dimensions) {
-    var down = "v" + dimensions.down;
-    var forward1 = "h" + dimensions.forward1;
-    var up = "v-" + dimensions.up;
     var forward2 = "h" + dimensions.forward2;
+
+    var x = this.points.from_x;
+    var y = this.points.from_y;
+
+    var down = new Line("down", {
+                 x: x,
+                 y: y,
+                 length: dimensions.down,
+                 prefix: "v"
+               });
+
+    x += CURVE_SPACING;
+    y += (down + CURVE_SPACING);
+    var forward1 = new Line("forward1", {
+                     x: x,
+                     y: y,
+                     length: dimensions.forward1,
+                     prefix: "h"
+                   });
+
+    x += (forward1 + CURVE_SPACING);
+    y -= CURVE_SPACING;
+    var up = new Line("up", {
+               x: x,
+               y: y,
+               length: dimensions.up,
+               prefix: "v-"
+             });
+
+    x += CURVE_SPACING;
+    y -= (up + CURVE_SPACING);
+    var forward2 = new Line("forward2", {
+                     x: x,
+                     y: y,
+                     length: dimensions.forward2,
+                     prefix: "v-"
+                   });
+
     this._dimensions.current = dimensions;
-    this._path = pathD(xy(this.points.from_x, this.points.from_y), down, CURVE_DOWN_RIGHT, forward1, CURVE_RIGHT_UP, up, CURVE_UP_RIGHT, forward2);
+    this._path = pathD(
+                   xy(this.points.from_x, this.points.from_y),
+                   down.path,
+                   CURVE_DOWN_RIGHT,
+                   forward1.path,
+                   CURVE_RIGHT_UP,
+                   up.path,
+                   CURVE_UP_RIGHT,
+                   forward2.path
+                 );
   }
 
   nudge(nF) {
