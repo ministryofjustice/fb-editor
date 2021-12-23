adb685abbb7f19bbd0bf3dd59d5df563a0a13927
brenetic <brendan.butler@digital.justice.gov.uk>
2021-12-23 16:37:18 +0000

    Remove unused module
    This was intended to be used with the Uptime Checks worker jobs but in
    the end was not necessary

--- app/services/environment_check.rb
+++
@@ -1,8 +0,0 @@
-module EnvironmentCheck
-  include ::Publisher::ConfigurationNaming
-  LIVE_PRODUCTION = 'live-production'.freeze
-
-  def live_production?(deployment_environment:, platform_environment: ENV['PLATFORM_ENV'])
-    "#{platform_environment}-#{deployment_environment}" == LIVE_PRODUCTION
-  end
-end
