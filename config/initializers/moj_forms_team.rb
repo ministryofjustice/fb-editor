dffe329b770f91909adb380860912e1a3fbdd6fc
brenetic <brendan.butler@digital.justice.gov.uk>
2022-01-10 10:19:13 +0000

    Add Chris Pymm to MoJ Forms devs group
    Also remove Tomas

--- config/initializers/moj_forms_team.rb
+++ config/initializers/moj_forms_team.rb
@@ -3,7 +3,7 @@ MOJ_FORMS_DEVS = [
   'matt.tei@digital.justice.gov.uk',
   'natalie.seeto@digital.justice.gov.uk',
   'steven.burnell@digital.justice.gov.uk',
-  'tomas.destefi@digital.justice.gov.uk'
+  'chris.pymm@digital.justice.gov.uk'
 ].freeze
 
 Rails.application.config.moj_forms_devs = MOJ_FORMS_DEVS
