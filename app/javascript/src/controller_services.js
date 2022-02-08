8e77c661b62613aa0cbfded133cb1d74a83d0250
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-02-08 11:29:00 +0000

    Fix condition to silence JS recursion error message

--- app/javascript/src/controller_services.js
+++ app/javascript/src/controller_services.js
@@ -902,7 +902,7 @@ function adjustOverlappingFlowConnectorPaths($overview) {
   var $paths = $overview.find(".FlowConnectorPath").not(".ForwardPath, .DownForwardPath"); // Filter out Paths we can ignore to save some processing time
   var somethingMoved;
   var numberOfPaths = $paths.length;
-  var keepChecking = true;
+  var keepChecking = $paths.length > 1;
   var loopCount = 1;
 
   do {
