63950ad7ba2497e6bb91188951065ba1c6ae2545
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-03-28 16:55:55 +0100

    Step 1 in refactor of JS tests for Branch (simplify and organise)

---
+++ test/branches/branch_constants.js
@@ -0,0 +1,30 @@
+module.exports = {
+  CLASSNAME_BRANCH_ERROR_MESSAGE: "test-error-message",
+  CLASSNAME_ERROR_1: "error-classname-1",
+  CLASSNAME_ERROR_2: "error-classname-2",
+
+  SELECTOR_PRE_BRANCH_DESTINATION: ".destination",
+  SELECTOR_PRE_BRANCH_CONDITION: ".condition",
+  SELECTOR_PRE_BRANCH_CONDITION_ADD: ".condition-injector",
+  SELECTOR_PRE_BRANCH_CONDITION_REMOVE: ".condition-remover",
+  SELECTOR_PRE_BRANCH_QUESTION: ".question",
+  SELECTOR_PRE_BRANCH_ANSWER: ".answer",
+  SELECTOR_PRE_BRANCH_REMOVE: ".branch-remover",
+
+  SELECTOR_BRANCH_DESTINATION: ".BranchDestination",
+  SELECTOR_BRANCH_CONDITION: ".BranchCondition",
+  SELECTOR_BRANCH_CONDITION_ADD: ".BranchConditionInjector",
+  SELECTOR_BRANCH_CONDITION_REMOVE: ".BranchConditionRemover",
+  SELECTOR_BRANCH_QUESTION: ".BranchQuestion",
+  SELECTOR_BRANCH_ANSWER: ".BranchAnswer",
+  SELECTOR_BRANCH_REMOVE: ".BranchRemover",
+
+  TEXT_BRANCH_REMOVE: "remove branch text",
+  TEXT_CONDITION_ADD: "add condition text",
+  TEXT_CONDITION_REMOVE: "remove condition text",
+  TEXT_ERROR_MESSAGE: "This is an error message",
+  TEXT_LABEL_QUESTION_AND: "And",
+  TEXT_LABEL_QUESTION_IF: "If",
+
+  URL_EXPRESSION: "/not/needed"
+}
