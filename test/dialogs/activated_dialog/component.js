cbead4e34b5875a74358bf70817f99a46db3dc91
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-04-11 19:27:46 +0100

    Test - should have the component class name present

--- test/dialogs/activated_dialog/component.js
+++ test/dialogs/activated_dialog/component.js
@@ -31,7 +31,11 @@ describe("ActivatedDialog", function() {
       expect($container.get(0)).to.equal($dialog.get(0));
     });
 
-    it("should have the component class name present");
+    it("should have the component class name present", function() {
+      var $container = created.$node.parent('[role=dialog]');
+      expect($container.hasClass(c.CLASSNAME_COMPONENT)).to.be.true;
+    });
+
     it("should apply CSS classnames passed in config");
     it("should use config.okText as button text");
     it("should store an onOk handler when passed in the config");
