a54998c6d7546d3949db693c1d4d486257f1e4f6
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-04-12 14:12:31 +0100

    Test - should open the dialog with public open() method

---
+++ test/dialogs/activated_dialog/methods.js
@@ -0,0 +1,40 @@
+require('../../setup');
+
+describe("ActivatedDialog", function() {
+
+  const helpers = require("./helpers.js");
+  const c = helpers.constants;
+  const COMPONENT_ID = "dialog-for-testing";
+
+
+  describe("Methods", function() {
+    // Note: Due to component makeup, the component is actually the
+    // parent/container element to original target $node.
+    var created;
+
+    before(function() {
+      helpers.setupView();
+      created = helpers.createDialog(COMPONENT_ID)
+    });
+
+    after(function() {
+      helpers.teardownView();
+      created.$node.remove();
+      created = {};
+    });
+
+
+    it("should open the dialog with public open() method", function() {
+      var $dialog = $("#" + COMPONENT_ID);
+      expect($dialog.css("display")).to.equal("none");
+      created.dialog.open();
+      expect($dialog.css("display")).to.not.equal("none");
+    });
+
+    it("should open the dialog on activator button press");
+    it("should be open on page load when config.autoOpen is true");
+    it("should be closed on page load when config.autoOpen is false");
+    it("should close the dialog on negative button press");
+  });
+
+});
