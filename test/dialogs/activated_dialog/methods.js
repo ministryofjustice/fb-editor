4d263f06c037874b68c048665957beaaee8736d2
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-04-12 14:44:13 +0100

    Test - should be closed on page load when config.autoOpen is false

--- test/dialogs/activated_dialog/methods.js
+++ test/dialogs/activated_dialog/methods.js
@@ -10,38 +10,55 @@ describe("ActivatedDialog", function() {
   describe("Methods", function() {
     // Note: Due to component makeup, the component is actually the
     // parent/container element to original target $node.
-    var created;
-
-    before(function() {
-      helpers.setupView();
-      created = helpers.createDialog(COMPONENT_ID);
-      $(document.body).append(created.dialog.activator.$node);
-    });
-
-    after(function() {
-      helpers.teardownView();
-      created.$node.remove();
-      created = {};
-    });
-
 
     it("should open the dialog with public open() method", function() {
+      var created = setup();
       var $dialog = $("#" + COMPONENT_ID);
+
       expect($dialog.css("display")).to.equal("none");
+
       created.dialog.open();
       expect($dialog.css("display")).to.not.equal("none");
+
+      teardown(created);
     });
 
-    it.only("should open the dialog on activator button press", function() {
-      var $dialog = $("#" + COMPONENT_ID);
+    it("should open the dialog on activator button press", function() {
+      var created = setup();
+      var $dialog;
+
+      $(document.body).append(created.dialog.activator.$node);
+
+      $dialog = $("#" + COMPONENT_ID);
       expect($dialog.css("display")).to.equal("none");
+
       $(".DialogActivator").click();
       expect($dialog.css("display")).to.not.equal("none");
+
+      teardown(created);
+    });
+
+    it("should be closed on page load when config.autoOpen is false", function() {
+      var created = setup();
+      var $dialog = $("#" + COMPONENT_ID);
+
+      expect($dialog.css("display")).to.equal("none");
+      teardown(created);
     });
 
     it("should be open on page load when config.autoOpen is true");
-    it("should be closed on page load when config.autoOpen is false");
     it("should close the dialog on negative button press");
   });
+
+  function setup() {
+    helpers.setupView();
+    return helpers.createDialog(COMPONENT_ID);
+  }
+
+  function teardown(created) {
+    helpers.teardownView();
+    created.$node.remove();
+    created = {};
+  }
 
 });
