b285b1260ca67694af0f5ce0c4b84aea7572320e
brenetic <brendan.butler@digital.justice.gov.uk>
2022-04-19 17:41:03 +0100

    Deny access to forms for unauthorised users
    Currently the Editor has two types of users:
    
    - Form creators
    - Admin users
    
    These are the only users that should be able to access forms, pages,
    branches and to be able to preview forms.
    
    In addition admin users are the only ones that should have access to the
    admin dashboard, but should retain access to the other parts of the
    Editor no matter who created the form.
    
    We skip checking for specific user authorisation on the all forms page
    as there are no specific services involved.
    
    The API endpoints need a bit more work. Some of the API Controllers
    inherit from the main apps BranchesController as it is used to setup
    some branch parameters. This does mean that the authorisation behaviour
    is a little different. For the main app we want to show an unauthorised
    page. However for the API endpoints we would want to respond with a
    standard unauthorised status.
    
    For this particular PR the endpoints are already requiring that a user
    is logged in. Only the Move endpoint affects any metadata and this will
    we addressed in another PR.
    
    For the remainder of the API endpoints we will revisit the authorisation
    in a different PR as it will be a fairly large restructuring.

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -1,3 +1,7 @@
 class PermissionsController < ApplicationController
+  include ApplicationHelper
+  include AuthorisationHelper
+
   before_action :require_user!
+  before_action :authorised_access
 end
---
+++ spec/requests/admin/admin_authorisation_spec.rb
@@ -0,0 +1,98 @@
+RSpec.describe 'Admin authorisation spec', type: :request do
+  let(:service) { MetadataPresenter::Service.new(metadata_fixture(:branching)) }
+  let(:current_user) { double(id: SecureRandom.uuid, email: 'ellen.ripley@nostromo.eg') }
+
+  before do
+    allow_any_instance_of(Admin::ApplicationController).to receive(:require_user!).and_return(true)
+    allow_any_instance_of(
+      Admin::ApplicationController
+    ).to receive(:current_user).and_return(current_user)
+  end
+
+  shared_examples 'an authorisation action' do
+    context 'when admin user' do
+      before do
+        allow_any_instance_of(
+          Admin::ApplicationController
+        ).to receive(:moj_forms_dev?).and_return(true)
+      end
+
+      it 'allows access' do
+        request
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'when not admin user' do
+      it 'does not allow access' do
+        expect(request).to redirect_to(unauthorised_path)
+      end
+    end
+  end
+
+  context 'when admin dashboard root' do
+    it_behaves_like 'an authorisation action' do
+      let(:request) { get admin_root_path }
+    end
+  end
+
+  context 'when overviews page' do
+    it_behaves_like 'an authorisation action' do
+      let(:request) { get '/admin/overviews' }
+    end
+  end
+
+  context 'when legacy service name page' do
+    it_behaves_like 'an authorisation action' do
+      let(:request) { get '/admin/legacy_service_names' }
+    end
+  end
+
+  context 'when uptime checks page' do
+    it_behaves_like 'an authorisation action' do
+      let(:request) { get '/admin/uptime_checks' }
+
+      before do
+        allow_any_instance_of(
+          Admin::UptimeChecksController
+        ).to receive(:services_without_uptime_checks).and_return([])
+        allow_any_instance_of(
+          Admin::UptimeChecksController
+        ).to receive(:with_uptime_checks).and_return([])
+        allow_any_instance_of(
+          Admin::UptimeChecksController
+        ).to receive(:non_editor_service_checks).and_return([])
+      end
+    end
+  end
+
+  context 'when services page' do
+    it_behaves_like 'an authorisation action' do
+      let(:request) { get '/admin/services' }
+      let(:all_services) do
+        {
+          services: [],
+          total_count: 0,
+          page: 1,
+          per_page: 10
+        }
+      end
+
+      before do
+        allow(MetadataApiClient::Service).to receive(:all_services).and_return(all_services)
+      end
+    end
+  end
+
+  context 'when users page' do
+    it_behaves_like 'an authorisation action' do
+      let(:request) { get '/admin/users' }
+    end
+  end
+
+  context 'when publish services page' do
+    it_behaves_like 'an authorisation action' do
+      let(:request) { get '/admin/publish_services' }
+    end
+  end
+end
