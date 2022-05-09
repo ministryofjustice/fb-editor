ebfe323f60be1fcfe496700482dd5eb558218158
brenetic <brendan.butler@digital.justice.gov.uk>
2022-05-09 16:03:17 +0100

    Move minimum validation into shared number validation example
    Minimum validation will also need to act like a number validation. When
    the maximum validation validator is added it will need to do the same
    thing so use a shared example here.

--- spec/models/component_validations/minimum_validation_spec.rb
+++ spec/models/component_validations/minimum_validation_spec.rb
@@ -17,56 +17,11 @@ RSpec.describe 'MinimumValidation' do
   let(:validator) { 'minimum' }
   let(:status) { 'enabled' }
   let(:value) { '5' }
+  let(:expected_number_error) { 'Minimum answer value needs to be a number' }
+  let(:expected_blank_error) { 'Enter an answer for Minimum answer value' }
 
   it_behaves_like 'a base component validation'
-
-  context 'validating a number' do
-    context 'when enabled and value is present' do
-      it 'validates the number value' do
-        expect_any_instance_of(NumberValidator).to receive(:validate)
-        subject.valid?
-      end
-
-      context 'when value is not a number' do
-        let(:value) { 'i am not number!' }
-        let(:expected_error) { 'Minimum answer value needs to be a number' }
-
-        before { subject.valid? }
-
-        it 'is invalid' do
-          expect(subject).to be_invalid
-        end
-
-        it 'surfaces the error correctly' do
-          expect(subject.errors.full_messages.first).to eq(expected_error)
-        end
-      end
-
-      context 'when value is not present' do
-        let(:value) { nil }
-        let(:expected_error) { 'Enter an answer for Minimum answer value' }
-
-        before { subject.valid? }
-
-        it 'is invalid' do
-          expect(subject).to be_invalid
-        end
-
-        it 'surfaces the error correctly' do
-          expect(subject.errors.full_messages.first).to eq(expected_error)
-        end
-      end
-    end
-
-    context 'when status attribute is not present' do
-      let(:status) { nil }
-
-      it 'does not validate the number value' do
-        expect_any_instance_of(NumberValidator).not_to receive(:validate)
-        subject.valid?
-      end
-    end
-  end
+  it_behaves_like 'a number validation'
 
   describe '#to_metadata' do
     let(:expected_metadata) { { 'minimum' => '5' } }
---
+++ spec/support/shared_examples/number_validation_examples.rb
@@ -0,0 +1,47 @@
+RSpec.shared_examples 'a number validation' do
+  context 'validating a number' do
+    context 'when enabled and value is present' do
+      it 'validates the number value' do
+        expect_any_instance_of(NumberValidator).to receive(:validate)
+        subject.valid?
+      end
+
+      context 'when value is not a number' do
+        let(:value) { 'i am not number!' }
+
+        before { subject.valid? }
+
+        it 'is invalid' do
+          expect(subject).to be_invalid
+        end
+
+        it 'surfaces the error correctly' do
+          expect(subject.errors.full_messages.first).to eq(expected_number_error)
+        end
+      end
+
+      context 'when value is not present' do
+        let(:value) { nil }
+
+        before { subject.valid? }
+
+        it 'is invalid' do
+          expect(subject).to be_invalid
+        end
+
+        it 'surfaces the error correctly' do
+          expect(subject.errors.full_messages.first).to eq(expected_blank_error)
+        end
+      end
+    end
+
+    context 'when status attribute is not present' do
+      let(:status) { nil }
+
+      it 'does not validate the number value' do
+        expect_any_instance_of(NumberValidator).not_to receive(:validate)
+        subject.valid?
+      end
+    end
+  end
+end
