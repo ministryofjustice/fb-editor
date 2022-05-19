d8111339b327d4b4f79d191accee4a8fc8e55dc3
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-05-19 13:42:04 +0100

    Make classname unique, correct classname param passed into method, and fix classname test

--- test/utilities/create_element_test.js
+++ test/utilities/create_element_test.js
@@ -4,16 +4,17 @@ const utilities = require('../../app/javascript/src/utilities.js');
 describe('utilities.createElement', function () {
 
   it('should create a new DOM node', function() {
-    var node = utilities.createElement('p', 'Some text here', ['classname1', 'classname2']);
-    var paragraphs = document.getElementsByTagName('p');
+    const UNIQUE_CLASSNAME = "utilities-create-element-method-injected-item";
+    const TEXT = "utilities createElement injected paragraph";
+    var node = utilities.createElement('p', TEXT, "classname1 " + UNIQUE_CLASSNAME);
+    var paragraphs = document.getElementsByClassName(UNIQUE_CLASSNAME);
     var classnames;
 
-    expect(paragraphs.length).to.equal(2);
-    expect(paragraphs[1].firstChild.nodeValue).to.equal('Some text here');
+    expect(paragraphs.length).to.equal(1);
+    expect(paragraphs[0].firstChild.nodeValue).to.equal(TEXT);
 
-    classnames = paragraphs[1].className.split(",");
-    expect(classnames).to.include('classname1');
-    expect(classnames).to.include('classname2');
+    expect(paragraphs[0].className).to.include('classname1');
+    expect(paragraphs[0].className).to.include(UNIQUE_CLASSNAME);
   });
 
 });
