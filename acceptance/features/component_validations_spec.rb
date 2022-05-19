f9bf475a3854ef204fc8792ce4dbfa7cfba116a3
brenetic <brendan.butler@digital.justice.gov.uk>
2022-05-19 15:57:19 +0100

    Add acceptance tests for the date_after and date_before validations

--- acceptance/features/component_validations_spec.rb
+++ acceptance/features/component_validations_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../spec_helper'
 
-feature 'Move a page' do
+feature 'Component validations' do
   let(:editor) { EditorApp.new }
   let(:service_name) { generate_service_name }
   let(:fixture) { 'all_component_types_fixture' }
@@ -106,6 +106,130 @@ feature 'Move a page' do
     )
   end
 
+  scenario 'date after validation' do
+    and_I_visit_a_page('Date')
+    when_I_want_to_select_question_properties
+    then_I_should_see_the_date_before_and_after_validations
+
+    and_I_select_a_validation(I18n.t('question.menu.date_after'))
+    then_I_should_see_the_validation_modal(
+      I18n.t('dialogs.component_validations.date_after.label'),
+      I18n.t('dialogs.component_validations.date_after.status_label')
+    )
+
+    and_I_enable_the_validation
+    and_I_set_the_date_input_value('day', '10')
+    and_I_set_the_date_input_value('month', '11')
+    click_button(I18n.t('dialogs.component_validations.button'))
+    then_I_should_see_an_error_message(
+      I18n.t(
+      'activemodel.errors.models.date_validation.missing_attribute',
+      label: 'Earliest date',
+      attribute: I18n.t('dialogs.component_validations.date.year').downcase
+    ))
+
+    and_I_set_the_date_input_value('year', 'this is not a year')
+    click_button(I18n.t('dialogs.component_validations.button'))
+    then_I_should_see_an_error_message(
+      I18n.t(
+      'activemodel.errors.models.date_validation.invalid',
+      label: I18n.t('dialogs.component_validations.date_after.label')
+    ))
+
+    and_I_set_the_date_input_value('year', '2001')
+    click_button(I18n.t('dialogs.component_validations.button'))
+    then_I_should_not_see_the_validation_modal(
+      I18n.t('dialogs.component_validations.date_after.label'),
+      I18n.t('dialogs.component_validations.date_after.status_label')
+    )
+
+    when_I_want_to_select_question_properties
+    and_I_select_a_validation(I18n.t('question.menu.date_after'))
+    then_I_should_see_the_previously_set_date_configuration('day', '10')
+    then_I_should_see_the_previously_set_date_configuration('month', '11')
+    then_I_should_see_the_previously_set_date_configuration('year', '2001')
+
+    and_I_set_the_date_input_value('day', '28')
+    and_I_set_the_date_input_value('month', '2')
+    and_I_set_the_date_input_value('year', '1999')
+    click_button(I18n.t('dialogs.component_validations.button'))
+
+    click_button(I18n.t('actions.save'))
+    when_I_want_to_select_question_properties
+    and_I_select_a_validation(I18n.t('question.menu.date_after'))
+    then_I_should_see_the_previously_set_date_configuration('day', '28')
+    then_I_should_see_the_previously_set_date_configuration('month', '02')
+    then_I_should_see_the_previously_set_date_configuration('year', '1999')
+    click_button(I18n.t('dialogs.button_cancel'))
+
+    and_I_return_to_flow_page
+    preview = when_I_preview_the_page('Date')
+    then_I_should_preview_the_date_page(
+      preview: preview,
+      first_date: { day: '1', month: '2', year: '1901' },
+      second_date: { day: '28', month: '2', year: '2001' },
+      error_message: 'Enter a later date for Date'
+    )
+  end
+
+  scenario 'date before validation' do
+    and_I_visit_a_page('Date')
+    when_I_want_to_select_question_properties
+    then_I_should_see_the_date_before_and_after_validations
+
+    and_I_select_a_validation(I18n.t('question.menu.date_before'))
+    then_I_should_see_the_validation_modal(
+      I18n.t('dialogs.component_validations.date_before.label'),
+      I18n.t('dialogs.component_validations.date_before.status_label')
+    )
+
+    and_I_enable_the_validation
+    and_I_set_the_date_input_value('month', '6')
+    and_I_set_the_date_input_value('year', '2050')
+    click_button(I18n.t('dialogs.component_validations.button'))
+    then_I_should_see_an_error_message(
+      I18n.t(
+      'activemodel.errors.models.date_validation.missing_attribute',
+      label: 'Latest date',
+      attribute: I18n.t('dialogs.component_validations.date.day').downcase
+    ))
+
+    and_I_set_the_date_input_value('day', '30')
+    click_button(I18n.t('dialogs.component_validations.button'))
+    then_I_should_not_see_the_validation_modal(
+      I18n.t('dialogs.component_validations.date_before.label'),
+      I18n.t('dialogs.component_validations.date_before.status_label')
+    )
+
+    when_I_want_to_select_question_properties
+    and_I_select_a_validation(I18n.t('question.menu.date_before'))
+    then_I_should_see_the_previously_set_date_configuration('day', '30')
+    then_I_should_see_the_previously_set_date_configuration('month', '06')
+    then_I_should_see_the_previously_set_date_configuration('year', '2050')
+
+    and_I_set_the_date_input_value('day', '14')
+    and_I_set_the_date_input_value('month', '8')
+    and_I_set_the_date_input_value('year', '2045')
+    click_button(I18n.t('dialogs.component_validations.button'))
+
+    click_button(I18n.t('actions.save'))
+    when_I_want_to_select_question_properties
+    and_I_select_a_validation(I18n.t('question.menu.date_before'))
+    then_I_should_see_the_previously_set_date_configuration('day', '14')
+    then_I_should_see_the_previously_set_date_configuration('month', '08')
+    then_I_should_see_the_previously_set_date_configuration('year', '2045')
+    click_button(I18n.t('dialogs.button_cancel'))
+
+    and_I_return_to_flow_page
+    preview = when_I_preview_the_page('Date')
+    then_I_should_preview_the_date_page(
+      preview: preview,
+      first_date: { day: '1', month: '2', year: '2050' },
+      second_date: { day: '28', month: '2', year: '2001' },
+      error_message: 'Enter an earlier date for Date'
+    )
+  end
+
   def and_I_visit_a_page(flow_title)
     editor.flow_thumbnail(flow_title).click
   end
@@ -120,10 +244,6 @@ feature 'Move a page' do
 
   def and_I_set_the_input_value(value)
     page.find(:css, 'input#component_validation_value').set(value)
-  end
-
-  def and_I_set_the_date_input_value(field, value)
-    page.find_field("component_validation[#{field}]").set(value)
   end
 
   def when_I_preview_the_page(flow_title)
@@ -164,6 +284,7 @@ feature 'Move a page' do
   end
 
   def then_I_should_see_the_previously_set_configuration(value)
+    sleep(0.5)
     expect(page.find(:css, 'input#component_validation_value').value).to eq(value)
   end
 
@@ -184,15 +305,7 @@ feature 'Move a page' do
   end
 
   def then_I_should_see_the_previously_set_date_configuration(field, value)
-    expect(page.find_field("component_validation[#{field}]").value).to eq(value)
-  end
-
-  def then_I_should_see_the_date_before_and_after_validations
-    expect(editor.text).to include(I18n.t('question.menu.date_before'))
-    expect(editor.text).to include(I18n.t('question.menu.date_after'))
-  end
-
-  def then_I_should_see_the_previously_set_date_configuration(field, value)
+    sleep(0.5)
     expect(page.find_field("component_validation[#{field}]").value).to eq(value)
   end
 
