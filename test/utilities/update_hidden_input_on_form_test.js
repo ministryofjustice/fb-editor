3ce1dfc3441f51e5e6bf8df70eedffe3ff644dde
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-05-19 14:10:56 +0100

    Tweak form ID code

--- test/utilities/update_hidden_input_on_form_test.js
+++ test/utilities/update_hidden_input_on_form_test.js
@@ -3,14 +3,16 @@ const utilities = require('../../app/javascript/src/utilities.js');
 
 
 describe('utilities.updateHiddenInputOnForm', function() {
+  const FORM_ID = "update-hidden-input-on-form-test";
+
   before(function() {
     var $form = $('<form></form>');
-    $form.attr('id', 'updateHiddenInputOnForm');
+    $form.attr('id', FORM_ID);
     $(document.body).append($form);
   });
 
   it('should update an existing hidden form element', function() {
-    var $form1 = $('#updateHiddenInputOnForm');
+    var $form1 = $("#" + FORM_ID);
     utilities.addHiddenInpuElementToForm($form1, 'field2', 'field-2-content');
 
     expect($form1.find('[name=\'field2\']').val()).to.equal('field-2-content');
@@ -21,6 +23,6 @@ describe('utilities.updateHiddenInputOnForm', function() {
   });
 
   after(function() {
-    $('#updateHiddenInputOnForm').remove();
+    $("#" + FORM_ID).remove();
   });
 });
