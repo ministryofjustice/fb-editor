fd04a6d5c3f89d71f9dc611bd17ecf8dae6077c6
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-05-19 12:05:49 +0100

    Tweak to improve used constants

--- test/utilities/add_hidden_input_element_to_form_test.js
+++ test/utilities/add_hidden_input_element_to_form_test.js
@@ -4,20 +4,23 @@ const utilities = require('../../app/javascript/src/utilities.js');
 
 
 describe('utilities.addHiddenInpuElementToForm', function() {
+  const ELEMENT_ID = "add-hidden-input-element-to-form-test";
+
   before(function() {
     var $form = $('<form></form>');
-    $form.attr('id', 'addHiddenInpuElementToForm');
+    $form.attr('id', ELEMENT_ID);
     $(document.body).append($form);
   });
 
   it('should add a new hidden form element to specified form if does not exist', function() {
-    var $form = $('#addHiddenInpuElementToForm');
-    utilities.addHiddenInpuElementToForm($form, 'field1', 'field-1-content');
+    const CONTENT = 'field-1-content';
+    var $form = $('#' + ELEMENT_ID);
+    utilities.addHiddenInpuElementToForm($form, 'field1', CONTENT);
 
-    expect($form.find('[name=\'field1\']').val()).to.equal('field-1-content');
+    expect($form.find('[name=\'field1\']').val()).to.equal(CONTENT);
   });
 
   after(function() {
-    $('#addHiddenInpuElementToForm').remove();
+    $('#' + ELEMENT_ID).remove();
   });
 });
