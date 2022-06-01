9e41f3bda842dceef60e79289709ebc4c1f80ca0
Chris Pymm <chris@chrispymm.co.uk>
2022-06-01 14:37:26 +0100

    add in tests for async code

--- test/dialogs/dialog_validation/methods_test.js
+++ test/dialogs/dialog_validation/methods_test.js
@@ -9,83 +9,85 @@ describe("DialogValidation", function() {
 
 
   describe("Methods", function() {
-    var created;
+    describe("Synchronous form", function() {
+      var created;
 
-    beforeEach(function() {
-      helpers.setupView(COMPONENT_ID, true);
-      created = helpers.createDialog(COMPONENT_ID, {});
-    });
-
-    afterEach(function() {
-      helpers.teardownView(COMPONENT_ID);
-      created = {};
-    });
-
-    /* TEST METHOD: open()
-     **/
-    it("should open the dialog", function() {
-      var $container = created.$node.parents("[role=dialog]");
-
-      // should be closed by default
-      expect($container.css("display")).to.equal("none");
-
-      // now test open()
-      created.dialog.open();
-      expect($container.css("display")).to.not.equal("none");
-    });
-
-
-    /* TEST METHOD: close()
-     **/
-    it("should close the dialog", function() {
-      var $container = created.$node.parents("[role=dialog]");
-
-      // First open and check.
-      created.dialog.open();
-      expect($container.css("display")).to.not.equal("none");
-
-      // Then see if close() properly changes the CSS value
-      created.dialog.close();
-      expect($container.css("display")).to.equal("none");
-    });
-
-
-    /* TEST METHOD: isOpen()
-     **/
-    it("should report false when dialog is closed", function() {
-      var $container = created.$node.parents("[role=dialog]");
-
-      // First try to make sure it's open
-      created.dialog.close();
-      expect($container.css("display")).to.equal("none");
-
-      // Then test the method
-      expect(created.dialog.isOpen()).to.be.false;
-    });
-
-    it("should report true when dialog is open", function() {
-      var $container = created.$node.parents("[role=dialog]");
-
-      // First try to make sure it's open
-      created.dialog.open();
-      expect($container.css("display")).to.not.equal("none");
-
-      // Then test the method
-      expect(created.dialog.isOpen()).to.be.true;
-    });
-    
-    /* TEST METHOD: submit()
-    **/
-    it('should submit the form syncronously', function() {
-      var submitted = false;
-
-      created.dialog.$form.on('submit', (e) => {
-        submitted = true;
+      beforeEach(function() {
+        helpers.setupView(COMPONENT_ID, true);
+        created = helpers.createDialog(COMPONENT_ID, {});
       });
 
-      created.dialog.submit();
-      expect(submitted).to.be.true;
-      expect(created.dialog.isOpen()).to.be.false;
+      afterEach(function() {
+        helpers.teardownView(COMPONENT_ID);
+        created = {};
+      });
+
+      /* TEST METHOD: open()
+**/
+      it("should open the dialog", function() {
+        var $container = created.$node.parents("[role=dialog]");
+
+        // should be closed by default
+        expect($container.css("display")).to.equal("none");
+
+        // now test open()
+        created.dialog.open();
+        expect($container.css("display")).to.not.equal("none");
+      });
+
+
+      /* TEST METHOD: close()
+**/
+      it("should close the dialog", function() {
+        var $container = created.$node.parents("[role=dialog]");
+
+        // First open and check.
+        created.dialog.open();
+        expect($container.css("display")).to.not.equal("none");
+
+        // Then see if close() properly changes the CSS value
+        created.dialog.close();
+        expect($container.css("display")).to.equal("none");
+      });
+
+
+      /* TEST METHOD: isOpen()
+**/
+      it("should report false when dialog is closed", function() {
+        var $container = created.$node.parents("[role=dialog]");
+
+        // First try to make sure it's open
+        created.dialog.close();
+        expect($container.css("display")).to.equal("none");
+
+        // Then test the method
+        expect(created.dialog.isOpen()).to.be.false;
+      });
+
+      it("should report true when dialog is open", function() {
+        var $container = created.$node.parents("[role=dialog]");
+
+        // First try to make sure it's open
+        created.dialog.open();
+        expect($container.css("display")).to.not.equal("none");
+
+        // Then test the method
+        expect(created.dialog.isOpen()).to.be.true;
+      });
+
+      /* TEST METHOD: submit()
+**/
+      it('should submit the form syncronously', function() {
+        var submitted = false;
+
+        created.dialog.$form.on('submit', (e) => {
+          submitted = true;
+        });
+
+        created.dialog.submit();
+        expect(submitted).to.be.true;
+        expect(created.dialog.isOpen()).to.be.false;
+        });
     });
 
     describe('Async Form', function() {
@@ -101,11 +103,11 @@ describe("DialogValidation", function() {
         helpers.teardownView(COMPONENT_ID);
         created = {};
       });
-  
+
       it('should submit the form asynchronously', function() {
         var data = {'key': 'value'};
         var successCallback = sinon.spy();
- 
+
         server.respondWith("POST", c.REMOTE_SUBMIT_URL, [
           200,
           { "Content-Type": "application/json"},
@@ -120,7 +122,7 @@ describe("DialogValidation", function() {
         created.dialog.submit();
 
         expect(successCallback).to.have.been.calledWith(data);
-   
+
       });
     });
 
