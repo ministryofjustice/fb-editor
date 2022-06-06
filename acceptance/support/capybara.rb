82a10d386f07f6785e5aac1854c5f62102f9f846
Chris Pymm <chris@chrispymm.co.uk>
2022-06-06 13:21:47 +0100

    wip

--- acceptance/support/capybara.rb
+++ acceptance/support/capybara.rb
@@ -1,6 +1,6 @@
 Capybara.register_driver :selenium do |app|
   chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |o|
-    o.add_argument '--headless'
+    # o.add_argument '--headless'
     o.add_argument '--no-sandbox'
     o.add_argument '--window-size=1920,1200'
   end
