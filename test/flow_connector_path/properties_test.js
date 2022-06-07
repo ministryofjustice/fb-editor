a85e07f720313da2a70357bd8c76bba5b561a9c5
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-06-07 14:09:25 +0100

    Add/Update component and property tests for FlowConnectorPath

---
+++ test/flow_connector_path/properties_test.js
@@ -0,0 +1,62 @@
+require("../setup");
+
+describe("FlowConnectorPath", function() {
+  const helpers = require("./helpers.js");
+  const c = helpers.constants;
+  const CONTAINER_ID = "flowconnectorpath-for-testing-properties-container";
+  const COMPONENT_ID = "flowconnectorpath-for-testing-properties-connector";
+
+  describe("Properties", function() {
+    var created;
+    const POINTS = {
+          from_x: 10,
+          from_y: 12,
+          to_x: 25,
+          to_y: 27,
+          via_x: 20
+        };
+
+    before(function() {
+      helpers.setupView(CONTAINER_ID);
+      created = helpers.createFlowConnectorPath(COMPONENT_ID, POINTS, {
+        container: $("#" + CONTAINER_ID),
+        from: c.FAKE_FLOW_ITEM_1,
+        to: c.FAKE_FLOW_ITEM_2,
+        top: 5,
+        bottom: 10
+      });
+
+      // Base class FlowConnectorPath does not call build() function from constructor.
+      created.path.build();
+    });
+
+    after(function() {
+      helpers.teardownView(CONTAINER_ID);
+      created = {};
+    });
+
+    it("should make FlowConnectorPath.id public", function() {
+      expect(created.path.id).to.exist;
+      expect(created.path.id).to.equal(COMPONENT_ID);
+    });
+
+    it("should make FlowConnectorPath.points public", function() {
+      expect(created.path.points).to.exist;
+      expect(created.path.points.from_x).to.equal(POINTS.from_x);
+      expect(created.path.points.from_y).to.equal(POINTS.from_y);
+    });
+
+    it("should make FlowConnectorPath.from public", function() {
+      expect(created.path.from).to.exist;
+      expect(created.path.from.id).to.exist;
+      expect(created.path.from.id).to.equal(c.FAKE_FLOW_ITEM_1.id);
+    });
+
+    it("should make FlowConnectorPath.to public", function() {
+      expect(created.path.to).to.exist;
+      expect(created.path.to.id).to.exist;
+      expect(created.path.to.id).to.equal(c.FAKE_FLOW_ITEM_2.id);
+    });
+
+  });
+});
