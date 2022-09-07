0575e56fbf27e0ca75813bcf65a66ffb520b8fef
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-09-07 17:56:37 +0100

    Fix short form <email@address.blah> format broken by sanitize-html

--- app/javascript/src/editable_components.js
+++ app/javascript/src/editable_components.js
@@ -833,7 +833,7 @@ function convertToHtml(markdown) {
  * 1. Something in makeMarkdown is adding <!-- --> markup to the result
  *    so we're trying to get rid of it.
  *
- * 2. sanitizeHTML() is altering automatic link syntax by removing
+ * 2. sanitize-html is altering automatic link syntax by removing
  *    everything in (including) angle brackets added by showdown.
  *
  *    e.g. [link text](<http://some.url/here>)
@@ -846,13 +846,24 @@ function convertToHtml(markdown) {
  *
  *         which give us the correct link element (url+text).
  *
- * 3. Converts unwanted HTML from input (when passed HTML or Markdown).
+ * 3. sanitize-html is removing <some@email.com> formatted markup that
+ *    is the recommended syntax in some documentation. This bit will
+ *    filter for that syntax and convert to $mailtosome@email.com$mailto
+ *    which will help to bypass sanitize-html (see step 5).
+ *
+ * 4. Converts unwanted HTML from input (when passed HTML or Markdown).
  *    Note: Because we're converting from Markup, we need to be careful
  *          about what is converted into entity or escaped form for
  *          that reason, we are trying to be minimalistic in approach.
  *
- * 4. Fix markdown to blockquote element conversion (broken by the
- *    sanitizeHTML() function converting bracket > into &gt; entity.
+ * 5. To follow step 3 (and this must happen after step 4), we now filter
+ *    for $mailtosome@email.com$mailto and replace with the original
+ *    angle brackets to reset to <some@email.com> markdown. It's worth
+ *    noting that, after saving, the markdown of <some@email.com> gets
+ *    converted to [some@email.com](mailto:some@email.com) anyway.
+ *
+ * 6. Fix markdown to blockquote element conversion (broken by the
+ *    sanitize-html) script converting bracket > into &gt; entity.
  **/
 function cleanInput(input) {
   // 1.
@@ -860,8 +871,12 @@ function cleanInput(input) {
   // 2.
   input = input.replace(/\]\(\<(.*?)\>\)/mig, "]($1)");
   // 3.
-  input = sanitizeHtml(input);
+  input = input.replace(/\<([\w\-]+@{1}[\w\-\.]+)>/mig, "$mailto$1$mailto");
   // 4.
+  input = sanitizeHtml(input);
+  // 5.
+  input = input.replace(/\$mailto([\w\-]+@{1}[\w\-\.]+)\$mailto/mig, "<$1>");
+  // 6.
   input = input.replace(/\n&gt;(\s{1}.*?\n)/mig, "\n>$1");
   return input;
 }
