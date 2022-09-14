cf286f4c62061dd92bb328761f97bb2b6254ca01
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-09-14 17:33:24 +0100

    Convert old ActivatedMenuContainer (properties tests) to new test format

---
+++ test/menus/activated_menu_container/properties_test.js
@@ -0,0 +1,34 @@
+require("../../setup");
+
+describe("ActivatedMenuContainer", function() {
+  const helpers = require("./helpers.js");
+  const c = helpers.constants;
+  const ID = "ActivatedMenuContainer-for-properties-test";
+
+  describe("Properties", function() {
+    var created;
+
+    before(function() {
+      helpers.setupView(ID);
+      created = helpers.createActivatedMenuContainer(ID);
+    });
+
+    after(function() {
+      helpers.teardownView(ID);
+      created = {};
+    });
+
+    it("should return the $node", function() {
+      expect(created.item.$node).to.exist;
+      expect(created.item.$node.length).to.equal(1);
+      expect(created.item.$node.get(0)).to.equal(created.$node.get(0));
+    });
+
+    it("should return the menu", function() {
+      expect(created.item).to.exist;
+      expect(created.item.menu).to.exist;
+      expect(created.item.menu.$node.length).to.equal(1);
+    });
+
+  });
+});
