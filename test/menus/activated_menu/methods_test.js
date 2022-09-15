bff3f8a33ba33a936e7aa3da9de044298bee2ff9
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2022-09-15 18:16:03 +0100

    Add missing test for menuisOpen()

--- test/menus/activated_menu/methods_test.js
+++ test/menus/activated_menu/methods_test.js
@@ -20,60 +20,81 @@ describe("ActivatedMenu", function() {
 
     /* TEST METHOD: open()
      **/
-    it("should open the menu by the open() method", function() {
-      expect(created.item.container.$node.get(0).style.display).to.equal("none");
-      created.item.open();
-      expect(created.item.container.$node.get(0).style.display).to.equal("");
+    describe("open()", function() {
+      it("should open the menu by the open() method", function() {
+        expect(created.item.container.$node.get(0).style.display).to.equal("none");
+        created.item.open();
+        expect(created.item.container.$node.get(0).style.display).to.equal("");
+      });
+
+      it("should set the state.open to true when open() is activated", function() {
+        expect(created.item.state.open).to.be.false;
+        created.item.open();
+        expect(created.item.state.open).to.be.true;
+      });
+
+      it("should add the class 'active' to the activator on open()", function() {
+        expect(created.item.state.open).to.be.false;
+        created.item.open();
+        expect(created.item.activator.$node.hasClass("active")).to.be.true;
+      });
+
+      // TODO: Test is on hold because we cannot use jsDom for testing position of elements.
+      it("should set the position values passed in open()");
     });
 
-    it("should set the state.open to true when open() is activated", function() {
-      expect(created.item.state.open).to.be.false;
-      created.item.open();
-      expect(created.item.state.open).to.be.true;
-    });
+    /* TEST METHOD: isOpen()
+     **/
+    describe("isOpen()", function() {
+      it("should return false when menu not open", function() {
+        expect(created.item.state.open).to.be.false;
+        expect(created.item.isOpen()).to.be.false;
+      });
 
-    it("should add the class 'active' to the activator on open()", function() {
-      expect(created.item.state.open).to.be.false;
-      created.item.open();
-      expect(created.item.activator.$node.hasClass("active")).to.be.true;
-    });
+      it("should return true when menu open", function() {
+        created.item.state.open = true;
+        expect(created.item.isOpen()).to.be.true;
 
-    // TODO: Test is on hold because we cannot use jsDom for testing position of elements.
-    it("should set the position values passed in open()");
+        // reset to closed
+        created.item.state.open = false;
+      });
+    });
 
 
     /* TEST METHOD: close()
      **/
-    it("should close the menu by the close() method", function() {
-      created.item.open();
-      expect(created.item.container.$node.get(0).style.display).to.equal("");
+    describe("close()", function() {
+      it("should close the menu by the close() method", function() {
+        created.item.open();
+        expect(created.item.container.$node.get(0).style.display).to.equal("");
 
-      created.item.close();
-      expect(created.item.container.$node.get(0).style.display).to.equal("none");
+        created.item.close();
+        expect(created.item.container.$node.get(0).style.display).to.equal("none");
+      });
+
+      it("should set the state.open to false when close() is activated", function() {
+        created.item.open();
+        expect(created.item.state.open).to.be.true;
+
+        created.item.close();
+        expect(created.item.state.open).to.be.false;
+      });
+
+      it("should place focus on the activator on close()", function() {
+        created.item.open();
+        expect(created.item.activator.$node.hasClass("active")).to.be.true;
+
+        created.item.close();
+        expect(created.item.activator.$node.hasClass("active")).to.be.true;
+        expect(document.activeElement).to.eql(created.item.activator.$node[0]);
+      });
+
+      // TODO: Test is on hold because we cannot use jsDom for testing position of elements.
+      it("should reset the position values on close()");
+
+      // TODO: Test is on hold because we cannot use jsDom for testing mouse events.
+      it("should close the menu on mouseout (not sure if can test)");
     });
-
-    it("should set the state.open to false when close() is activated", function() {
-      created.item.open();
-      expect(created.item.state.open).to.be.true;
-
-      created.item.close();
-      expect(created.item.state.open).to.be.false;
-    });
-
-    it("should place focus on the activator on close()", function() {
-      created.item.open();
-      expect(created.item.activator.$node.hasClass("active")).to.be.true;
-
-      created.item.close();
-      expect(created.item.activator.$node.hasClass("active")).to.be.true;
-      expect(document.activeElement).to.eql(created.item.activator.$node[0]);
-    });
-
-    // TODO: Test is on hold because we cannot use jsDom for testing position of elements.
-    it("should reset the position values on close()");
-
-    // TODO: Test is on hold because we cannot use jsDom for testing mouse events.
-    it("should close the menu on mouseout (not sure if can test)");
 
   });
 });
