9f89bea9eb42644e80ef0edeb8eda3cd8c07804f
Natalie Seeto <njseeto@users.noreply.github.com>
2022-09-22 16:42:16 +0100

    Move shared FromAddress methods into a FromAddressObjects module
    These methods are now used in two controllers. Move the common methods
    out into a shared module to dry up the code.

--- app/controllers/api/from_address_controller.rb
+++ app/controllers/api/from_address_controller.rb
@@ -1,14 +1,10 @@
 module Api
   class FromAddressController < ApiController
+    include FromAddressObjects
+
     before_action :assign_from_address
 
     def resend_validation
-      from_address_creation = FromAddressCreation.new(
-        from_address: @from_address,
-        from_address_params: {},
-        email_service: email_service
-      )
-
       if from_address_creation.resend_validation
         head :ok
       else
@@ -16,18 +12,8 @@ module Api
       end
     end
 
-    private
-
-    def assign_from_address
-      @from_address = FromAddress.find_or_initialize_by(service_id: service.service_id)
-    end
-
-    def email_service
-      @email_service ||= if Rails.env.production?
-                           EmailService::Adapters::AwsSesClient.new
-                         else
-                           EmailService::Adapters::Local.new
-                         end
+    def from_address_params
+      {}
     end
   end
 end
