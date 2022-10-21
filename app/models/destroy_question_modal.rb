b0d35aec841d539c7f7734ed797edc2b06b3b13a
Natalie Seeto <njseeto@users.noreply.github.com>
2022-10-21 16:22:20 +0100

    Add delete modal for email questions
    Similar to blocking on a page level, we want to prevent users from
    deleting the specific email component question on a mulitple
    question page.
    
    Since this modal triggers on a component level rather than a page
    level, users will be able to delete other components on their
    mulitple question page.

--- app/models/destroy_question_modal.rb
+++ app/models/destroy_question_modal.rb
@@ -6,6 +6,7 @@ class DestroyQuestionModal
 
   def to_partial_path
     return 'api/questions/cannot_delete_modal' if can_not_be_deleted?
+    return 'api/questions/cannot_delete_confirmation_email_modal' if used_in_confirmation_email?
 
     'api/questions/destroy_message_modal'
   end
@@ -14,5 +15,18 @@ class DestroyQuestionModal
 
   def can_not_be_deleted?
     expressions.map(&:component).include?(question.uuid)
+  end
+
+  def used_in_confirmation_email?
+    return if confirmation_email_component_id.nil?
+
+    confirmation_email_component_id.decrypt_value.include?(question.id)
+  end
+
+  def confirmation_email_component_id
+    @confirmation_email_component_id ||= ServiceConfiguration.find_by(
+      service_id: service.service_id,
+      name: 'CONFIRMATION_EMAIL_COMPONENT_ID'
+    )
   end
 end
