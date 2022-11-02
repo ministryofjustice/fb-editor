082575863b925bc61fdb2f92180307de339d63ff
Chris Pymm <chris@chrispymm.co.uk>
2022-11-02 16:54:41 +0000

    Remove unneeded webpack config

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -8,7 +8,6 @@ const FormAnalyticsController = require('./controller_form_analytics');
 const FromAddressController = require('./controller_from_address');
 const CollectionEmailController = require('./controller_collection_email');
 const ConfirmationEmailController = require('./controller_confirmation_email');
-const SentryLogger = require('./sentry_logger');
 
 const {
   snakeToPascalCase,
