c8722f18f4f31b3f54f5653a3b483d3ba7336635
Chris Pymm <chris@chrispymm.co.uk>
2022-11-04 14:35:25 +0000

    Add components helpers and use them

---
+++ app/helpers/moj_forms_components_helper.rb
@@ -0,0 +1,14 @@
+module MojFormsComponentsHelper
+  {
+    mojf_settings_screen: 'MojForms::SettingsScreenComponent',
+    mojf_back_link: 'MojForms::BackLinkComnponent'
+  }.each do |name, klass|
+    define_method(name) do |*args, **kwargs, &block|
+      capture do
+        render(klass.constantize.new(*args, **kwargs)) do |com|
+          block.call(com) if block.present?
+        end
+      end
+    end
+  end
+end
