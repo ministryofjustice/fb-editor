36e62cdeda2d4d5585f5b973fe3cfb15a4a6c4ef
Natalie Seeto <njseeto@users.noreply.github.com>
2022-12-02 10:08:06 +0000

    Bump to presenter 2.17.25
    Also, change method name to show the result will be a boolean.

--- app/helpers/content_substitutor_helper.rb
+++ app/helpers/content_substitutor_helper.rb
@@ -2,11 +2,11 @@ module ContentSubstitutorHelper
   def content_substitutor
     @content_substitutor ||= ContentSubstitutor.new(
       service_name: service.service_name,
-      reference_number_enabled: reference_number_enabled
+      reference_number_enabled: reference_number_enabled?
     )
   end
 
-  def reference_number_enabled
+  def reference_number_enabled?
     ServiceConfiguration.where(
       service_id: service.service_id,
       name: 'REFERENCE_NUMBER'
