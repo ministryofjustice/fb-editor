dfecf317661865e098ab96f0a89199006c06b434
Chris Pymm <chris@chrispymm.co.uk>
2022-12-07 21:03:05 +0000

    Resolv not part of api module

--- app/controllers/api/first_publish_controller.rb
+++ app/controllers/api/first_publish_controller.rb
@@ -9,11 +9,11 @@ module Api
 
       url = PublishServicePresenter.new(publishes, service).url
       if url
-        dns = Resolv::DNS.new(nameserver: '1.1.1.1')
+        dns = ::Resolv::DNS.new(nameserver: '1.1.1.1')
         dns.getaddress(url)
         head :ok
       end
-    rescue Resolv::ResolvError
+    rescue ::Resolv::ResolvError
       head :not_found
     end
   end
