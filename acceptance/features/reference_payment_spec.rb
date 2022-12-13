1e78dfce1c2787fafadfe8ad33443cb1793d8725
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2022-12-13 19:01:40 +0000

    Reference number and payment link acceptance tests
    This PR has tests for:
    - Payment link validations (invalid url, blank url, reference number
    not checked)
    - Only reference number is checked
    - Happy payment link path (payment and reference checked and url valid)
    - Confirmation page styling and content changes (payment link enabled vs
    only reference number enabled)

---
+++ acceptance/features/reference_payment_spec.rb
@@ -0,0 +1,166 @@
+require_relative '../spec_helper'
+
+feature 'Reference Payment Page' do
+  let(:editor) { EditorApp.new }
+  let(:service_name) { generate_service_name }
+  let(:start_page) { 'Service name goes here' }
+  let(:payment_link_checkbox) { 'input#reference-payment-settings-payment-link-1-field' }
+  let(:payment_link_field) { 'input#reference-payment-settings-payment-link-url-field' }
+  let(:reference_number_checkbox) { 'input#reference-payment-settings-reference-number-1-field' }
+  let(:payment_link_label) { I18n.t('settings.payment_link.label') }
+  let(:valid_url) { I18n.t('activemodel.errors.models.reference_payment_settings.link_start_with') }
+  let(:confirmation_page_payment_text) { "You still need to pay\nYour reference number is:" }
+  let(:confirmation_warning_title) { I18n.t('settings.reference_number.confirmation_email_warning.title') }
+
+  background do
+    given_I_am_logged_in
+    given_I_have_a_service_fixture(name: service_name)
+    when_I_visit_the_reference_payment_page
+    then_I_should_see_the_payment_link_settings_configuration
+  end
+
+  scenario 'enable reference number only' do
+    with_setting(reference_number_checkbox, true)
+    click_button(I18n.t('actions.save'))
+    then_I_should_not_see_my_content(I18n.t('activemodel.errors.summary_title'))
+    expect(page).to have_css('.govuk-notification-banner__content', text: confirmation_warning_title)
+    then_checkbox_should_remain_checked(reference_number_checkbox)
+
+    then_I_add_a_page_with_email_component
+    when_I_visit_the_confirmation_email_settings_page
+    when_I_enable_confirmation_email('dev')
+    click_button(I18n.t('settings.submission.dev.save_button'))
+    when_I_enable_confirmation_email('production')
+    click_button(I18n.t('settings.submission.production.save_button'))
+    when_I_visit_the_reference_payment_page
+    expect(page).to_not have_css('.govuk-notification-banner__content', text: confirmation_warning_title)
+  end
+
+  scenario 'configure payment link settings without reference number' do
+    with_setting(payment_link_checkbox, true)
+    then_I_should_see_text(payment_link_label)
+    with_setting(payment_link_field, valid_url)
+    click_button(I18n.t('actions.save'))
+    then_I_should_see_text(I18n.t('activemodel.errors.models.reference_payment_settings.reference_number_disabled'))
+    then_checkbox_should_remain_checked(payment_link_checkbox)
+  end
+
+  scenario 'configure payment link settings without url' do
+    with_setting(reference_number_checkbox, true)
+    with_setting(payment_link_checkbox, true)
+    then_I_should_see_text(payment_link_label)
+    click_button(I18n.t('actions.save'))
+    then_I_should_see_text(I18n.t('activemodel.errors.models.reference_payment_settings.missing_payment_link'))
+    then_checkbox_should_remain_checked(reference_number_checkbox)
+    then_checkbox_should_remain_checked(payment_link_checkbox)
+  end
+
+  scenario 'payment links with invalid url' do
+    with_setting(reference_number_checkbox, true)
+    with_setting(payment_link_checkbox, true)
+    then_I_should_see_text(payment_link_label)
+    with_setting(payment_link_field, 'dummy-url.com')
+    click_button(I18n.t('actions.save'))
+    then_I_should_see_text(
+      I18n.t(
+        'activemodel.errors.models.reference_payment_settings.invalid_payment_url',link_start_with: valid_url
+      )
+    )
+    then_checkbox_should_remain_checked(reference_number_checkbox)
+    then_checkbox_should_remain_checked(payment_link_checkbox)
+  end
+
+  scenario 'email body in confirmation email settings page updates correctly' do
+    with_setting(reference_number_checkbox, true)
+    with_setting(payment_link_checkbox, true)
+    then_I_should_see_text(payment_link_label)
+    with_setting(payment_link_field, valid_url)
+    click_button(I18n.t('actions.save'))
+    then_I_add_a_page_with_email_component
+    when_I_visit_the_confirmation_email_settings_page
+    when_I_enable_confirmation_email('dev')
+    expect(page).to have_content('{{reference_number}}')
+    expect(page).to have_content('{{payment_link}}')
+
+    when_I_visit_the_reference_payment_page
+    with_setting(payment_link_checkbox, false)
+    click_button(I18n.t('actions.save'))
+    when_I_visit_the_confirmation_email_settings_page
+    when_I_enable_confirmation_email('dev')
+    expect(page).to_not have_content('{{payment_link}}')
+    expect(page).to have_content('{{reference_number}}')
+
+    when_I_visit_the_reference_payment_page
+    with_setting(reference_number_checkbox, false)
+    with_setting(payment_link_checkbox, false)
+    click_button(I18n.t('actions.save'))
+    when_I_visit_the_confirmation_email_settings_page
+    when_I_enable_confirmation_email('dev')
+    expect(page).to_not have_content('{{reference_number}}')
+    expect(page).to_not have_content('{{payment_link}}')
+  end
+
+  scenario 'confirmation page styling updates correctly' do
+    with_setting(reference_number_checkbox, true)
+    with_setting(payment_link_checkbox, true)
+    then_I_should_see_text(payment_link_label)
+    with_setting(payment_link_field, valid_url)
+    click_button(I18n.t('actions.save'))
+
+    and_I_return_to_flow_page
+    given_I_edit_a_confirmation_page
+    expect(page).to have_css('div.govuk-panel--confirmation-payment', text: confirmation_page_payment_text)
+
+    when_I_visit_the_reference_payment_page
+    with_setting(payment_link_checkbox, false)
+    click_button(I18n.t('actions.save'))
+    and_I_return_to_flow_page
+    given_I_edit_a_confirmation_page
+    expect(page).to have_css('div.govuk-panel--confirmation', text: "Application complete\nYour reference number is:")
+    then_I_should_not_see_text('You still need to pay')
+
+    when_I_visit_the_reference_payment_page
+    with_setting(payment_link_checkbox, false)
+    with_setting(reference_number_checkbox, false)
+    click_button(I18n.t('actions.save'))
+    and_I_return_to_flow_page
+    given_I_edit_a_confirmation_page
+    expect(page).to have_css('div.govuk-panel--confirmation', text: "Application complete")
+    then_I_should_not_see_text(confirmation_page_payment_text)
+  end
+
+  ## Reference Payment Settings Page
+  def when_I_visit_the_reference_payment_page
+    page.find(:css, '#main-content', visible: true)
+    editor.click_link(I18n.t('settings.name'))
+    expect(page).to have_content(I18n.t('settings.reference_number.lede'))
+    editor.click_link(I18n.t('settings.reference_number.heading'))
+  end
+
+  def then_I_should_see_the_payment_link_settings_configuration
+    expect(page).to have_content(I18n.t('settings.reference_number.heading'))
+    expect(page).to have_content(I18n.t('settings.reference_number.description', href:'user guide' ))
+    expect(page).to have_content(I18n.t('settings.reference_number.hint'))
+    expect(page).to have_content(I18n.t('settings.payment_link.legend'))
+    expect(page).to have_content(I18n.t('settings.payment_link.hint', href:'GOV.UK Pay account'))
+  end
+
+  def with_setting(setting, value)
+    page.find(:css, setting, visible: false).set(value)
+  end
+
+  def then_checkbox_should_remain_checked(attribute)
+    element = find(attribute, visible: false)
+    expect(element.checked?).to eq(true)
+  end
+
+  ## Pages Flow
+  def and_I_edit_the_question
+    editor.question_heading.first.set('New Email Component')
+  end
+
+  def when_I_update_the_question_name
+    and_I_edit_the_question
+    when_I_save_my_changes
+  end
+end
