a260a3e4312a0cc7b78f72eb8dbff76130c0851d
brenetic <brenetic@users.noreply.github.com>
2022-12-19 15:17:48 +0000

    Set min and max length form names as constants

--- app/services/editor/service.rb
+++ app/services/editor/service.rb
@@ -3,8 +3,11 @@ module Editor
     include ActiveModel::Model
     attr_accessor :service_name, :current_user, :service_id, :latest_metadata
 
+    MINIMUM = 3
+    MAXIMUM = 57
+
     validates :service_name, presence: true
-    validates :service_name, length: { minimum: 3, maximum: 57 }, allow_blank: true
+    validates :service_name, length: { minimum: MINIMUM, maximum: MAXIMUM }, allow_blank: true
     validates :service_name, format: { with: /\A[a-zA-Z][\sa-zA-Z0-9-'’()]*\z/ }, allow_blank: true
     validates :service_name, legacy_service_name: true
 
