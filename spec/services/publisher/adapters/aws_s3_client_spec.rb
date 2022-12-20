78c12c8952155046588b92a0888d720cfb5439f5
brenetic <brenetic@users.noreply.github.com>
2022-12-20 15:52:26 +0000

    Correctly inject S3 credentials into the Editor deployment
    We now inject the AWS S3 credentials and bucket names for all the
    environments using the secrets configuration.
    
    We can remove the need for the 'TEST' and 'LIVE' from the injected
    environment variable names. We only need 'DEV' and 'PRODUCTION' in order
    to distinguish between the different bucket and which credentials to use
    during publishing.

--- spec/services/publisher/adapters/aws_s3_client_spec.rb
+++ spec/services/publisher/adapters/aws_s3_client_spec.rb
@@ -1,18 +1,16 @@
 RSpec.describe Publisher::Adapters::AwsS3Client do
   subject(:aws_s3_client) do
-    described_class.new(platform_deployment)
+    described_class.new(
+      bucket: bucket,
+      access_key_id: 'some-access-key_id',
+      secret_access_key: 'some-secret-access-key'
+    )
   end
-  let(:platform_deployment) { 'TEST_DEV' }
 
   describe '#upload' do
     let(:body) { { foo: 'bar' } }
     let(:bucket) { 'some-bucket' }
     let(:object_key) { 'some-object-key' }
-
-    before do
-      allow(ENV).to receive(:[])
-      allow(ENV).to receive(:[]).with("AWS_S3_BUCKET_#{platform_deployment}").and_return(bucket)
-    end
 
     it 'should upload an object to s3' do
       expect_any_instance_of(Aws::S3::Client).to receive(:put_object).with(
