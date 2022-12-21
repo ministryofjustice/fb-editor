6c4959a4dca2da80f63f09c65ae9226d0e78a999
Chris Pymm <chris@chrispymm.co.uk>
2022-12-21 16:21:51 +0000

    Updated overlap calculations - now with no remaining overlaps
    Unfortunately in some circumstances we do need the 'via' points to be used in the bounds() calculation
    for a FlowConnectorPath.  This does result in many more intersections, increasing the calculation time
    but in order to guarantee no overlaps we need to do it! Still ~5x faster than originally though!

--- test/flow_connector_line/methods_test.js
+++ test/flow_connector_line/methods_test.js
@@ -18,55 +18,52 @@ describe("FlowConnectorLine", function() {
       created = {};
     });
 
-    /* TEST METHOD:  prop()
-     **/
 
-    it("should return overlapAllowed value", function() {
-      expect(created.lines[0].prop("overlapAllowed")).to.be.true;
-      expect(created.lines[1].prop("overlapAllowed")).to.be.false;
-      expect(created.lines[2].prop("overlapAllowed")).to.be.true;
+    describe('prop', function() {
+      it("should return overlapAllowed value", function() {
+        expect(created.lines[0].prop("overlapAllowed")).to.be.true;
+        expect(created.lines[1].prop("overlapAllowed")).to.be.false;
+        expect(created.lines[2].prop("overlapAllowed")).to.be.true;
+      });
+
+      it("should return x value", function() {
+        expect(created.lines[0].prop("x")).to.equal(1451);
+        expect(created.lines[1].prop("x")).to.equal(1531);
+        expect(created.lines[2].prop("x")).to.equal(1541);
+      });
+
+      it("should return y value", function() {
+        expect(created.lines[0].prop("y")).to.equal(313);
+        expect(created.lines[1].prop("y")).to.equal(303);
+        expect(created.lines[2].prop("y")).to.equal(63);
+      });
+
+      it("should return length value", function() {
+        expect(created.lines[0].prop("length")).to.equal(70);
+        expect(created.lines[1].prop("length")).to.equal(230);
+        expect(created.lines[2].prop("length")).to.equal(-2);
+      });
     });
 
-    it("should return x value", function() {
-      expect(created.lines[0].prop("x")).to.equal(1451);
-      expect(created.lines[1].prop("x")).to.equal(1531);
-      expect(created.lines[2].prop("x")).to.equal(1541);
+    describe('testOnlySvg', function() {
+      it("should return svg element", function() {
+        expect(created.lines[0].testOnlySvg().get(0).tagName).to.equal("svg");
+        expect(created.lines[1].testOnlySvg().get(0).tagName).to.equal("svg");
+        expect(created.lines[2].testOnlySvg().get(0).tagName).to.equal("svg");
+      });
+
+      it("should have component class on element", function() {
+        expect(created.lines[0].testOnlySvg().hasClass("FlowConnectorLine")).to.be.true;
+        expect(created.lines[1].testOnlySvg().hasClass("FlowConnectorLine")).to.be.true;
+        expect(created.lines[2].testOnlySvg().hasClass("FlowConnectorLine")).to.be.true;
+      });
+
+      it("should have red stroke for testing path", function() {
+        expect(created.lines[0].testOnlySvg().children("path").eq(0).get(0).style.stroke).to.equal("red");
+        expect(created.lines[1].testOnlySvg().children("path").eq(0).get(0).style.stroke).to.equal("red");
+        expect(created.lines[2].testOnlySvg().children("path").eq(0).get(0).style.stroke).to.equal("red");
+      });
     });
-
-    it("should return y value", function() {
-      expect(created.lines[0].prop("y")).to.equal(313);
-      expect(created.lines[1].prop("y")).to.equal(303);
-      expect(created.lines[2].prop("y")).to.equal(63);
-    });
-
-    it("should return length value", function() {
-      expect(created.lines[0].prop("length")).to.equal(70);
-      expect(created.lines[1].prop("length")).to.equal(230);
-      expect(created.lines[2].prop("length")).to.equal(-2);
-    });
-
-
-    /* TEST METHOD:  testOnlySvg()
-     **/
-
-    it("should return svg element", function() {
-      expect(created.lines[0].testOnlySvg().get(0).tagName).to.equal("svg");
-      expect(created.lines[1].testOnlySvg().get(0).tagName).to.equal("svg");
-      expect(created.lines[2].testOnlySvg().get(0).tagName).to.equal("svg");
-    });
-
-    it("should have component class on element", function() {
-      expect(created.lines[0].testOnlySvg().hasClass("FlowConnectorLine")).to.be.true;
-      expect(created.lines[1].testOnlySvg().hasClass("FlowConnectorLine")).to.be.true;
-      expect(created.lines[2].testOnlySvg().hasClass("FlowConnectorLine")).to.be.true;
-    });
-
-    it("should have red stroke for testing path", function() {
-      expect(created.lines[0].testOnlySvg().children("path").eq(0).get(0).style.stroke).to.equal("red");
-      expect(created.lines[1].testOnlySvg().children("path").eq(0).get(0).style.stroke).to.equal("red");
-      expect(created.lines[2].testOnlySvg().children("path").eq(0).get(0).style.stroke).to.equal("red");
-    });
-
 
   });
 
