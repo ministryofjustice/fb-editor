9abfdcdda87db8617f1a9ec5a812a5e8ef4af955
Natalie Seeto <njseeto@users.noreply.github.com>
2023-01-24 17:32:38 +0000

    Add OSPT to the domain allow list
    This has been requested via our support channels.

--- config/initializers/allowed_domains.rb
+++ config/initializers/allowed_domains.rb
@@ -2,7 +2,8 @@ ALLOWED_DOMAINS = [
   'justice.gov.uk',
   'digital.justice.gov.uk',
   'cica.gov.uk',
-  'judicialappointments.gov.uk'
+  'judicialappointments.gov.uk',
+  'ospt.gov.uk'
 ].freeze
 
 Rails.application.config.allowed_domains = ALLOWED_DOMAINS
