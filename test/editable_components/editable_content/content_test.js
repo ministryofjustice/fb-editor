a71d185fe1eb3559e7244b187d79d7b88c5c2199
Chris Pymm <chris@chrispymm.co.uk>
2023-02-16 21:31:54 +0000

    Add content transformation tests for EditableContent

---
+++ test/editable_components/editable_content/content_test.js
@@ -0,0 +1,141 @@
+require('../../setup');
+
+describe('EditableContent', function() {
+  const helpers = require('./helpers');
+  const c = helpers.constants;
+  const COMPONENT_ID = 'editable-content-content-test';
+  const COMPONENT_CLASSNAME = 'EditableContent';
+
+  const DIRTY_MARKDOWN = `
+# My content
+
+This is my lovely content. It has problems.
+
+Here is a link [click me](<https://google.com>).
+
+Here is an email address <email@address.com> it should come out as a link
+
+> This is a blockquote.
+
+Followed by another paragraph
+
+## What about a list or two?
+
+- Item 1
+- Item 2
+- Item 3
+
+1. First
+2. Second
+3. Third
+
+And one final paragraph.
+
+`
+
+  const CLEAN_MARKDOWN = `# My content
+
+This is my lovely content. It has problems.
+
+Here is a link [click me](https://google.com).
+
+Here is an email address <email@address.com> it should come out as a link
+
+> This is a blockquote.
+
+Followed by another paragraph
+
+## What about a list or two?
+
+- Item 1
+- Item 2
+- Item 3
+
+1. First
+2. Second
+3. Third
+
+And one final paragraph.`
+
+  const CLEAN_HTML = `<h1>My content</h1>
+<p>This is my lovely content. It has problems.</p>
+<p>Here is a link <a href="https://google.com">click me</a>.</p>
+<p>Here is an email address <a href="mailto:email@address.com">email@address.com</a> it should come out as a link</p>
+<blockquote>
+  <p>This is a blockquote.</p>
+</blockquote>
+<p>Followed by another paragraph</p>
+<h2>What about a list or two?</h2>
+<ul>
+<li>Item 1</li>
+<li>Item 2</li>
+<li>Item 3</li>
+</ul>
+<ol>
+<li>First</li>
+<li>Second</li>
+<li>Third</li>
+</ol>
+<p>And one final paragraph.</p>`
+
+  const getHtml = () => {
+    return $(document).find('.EditableContent .output').html();
+  }
+
+  describe('Content', function() {
+    var created;
+
+    beforeEach(function() {
+      created = helpers.createEditableContent(COMPONENT_ID);
+    });
+
+    afterEach(function() {
+      helpers.teardownView(COMPONENT_ID);
+      created = undefined;
+    });
+
+    it('should remove <!-- --> from content', function() {
+      created.instance.$input.val(DIRTY_MARKDOWN);
+      created.instance.update();
+      let html = getHtml();
+      expect(created.instance.markdown).to.not.include('<!-- -->');
+    });
+
+    it('should remove angle brackets from links created by showdown', function() {
+      created.instance.$input.val(DIRTY_MARKDOWN);
+      created.instance.update();
+      expect(created.instance.markdown).to.not.include('[click me](<https://google.com>)');
+      expect(created.instance.markdown).to.include('[click me](https://google.com)');
+    });
+
+    it('should contain mailto link <email@address.com>', function() {
+      created.instance.$input.val(DIRTY_MARKDOWN);
+      created.instance.update();
+      expect(created.instance.markdown).to.not.include('$mailtoemail@address.com$mailto');
+      expect(created.instance.markdown).to.include('<email@address.com>');
+    });
+
+    it('should contain correct markdown blockquote syntax', function() {
+      created.instance.$input.val(DIRTY_MARKDOWN);
+      created.instance.update();
+      expect(created.instance.markdown).to.not.include('&gt; This is a blockquote');
+      expect(created.instance.markdown).to.include('> This is a blockquote');
+
+    });
+
+
+    it('should output correct markdown', function() {
+      created.instance.$input.val(DIRTY_MARKDOWN);
+      created.instance.update();
+      expect(created.instance.markdown).to.equal(CLEAN_MARKDOWN);
+    });
+
+    it('should output correct html', function() {
+      created.instance.$input.val(DIRTY_MARKDOWN);
+      created.instance.update();
+      let html = getHtml();
+      expect(html).to.equal(CLEAN_HTML);
+    });
+
+  });
+});
