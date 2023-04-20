4bd1f89f28167a9de589de5d99959edcb2589b91
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-04-20 13:23:22 +0100

    Updating views controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,7 @@ Rails.application.routes.draw do
       post '/republish/:publish_service_id/:deployment_environment',
         to: 'services#republish', as: :republish
 
-
-      get '/api_submission_settings', to: 'api_submission#show'
-      put '/api_submission_settings', to: 'api_submission#update'
+      resources :api_submission, only: [:create, :index, :show, :edit, :update]
       resources :versions, only: [:update, :edit, :show]
     end
     resources :users
