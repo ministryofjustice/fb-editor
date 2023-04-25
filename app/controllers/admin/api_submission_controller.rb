789d6c82603941e6d2b17d8b7c0cfbb06161843f
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-04-25 11:55:25 +0100

    Using create route

--- app/controllers/admin/api_submission_controller.rb
+++ app/controllers/admin/api_submission_controller.rb
@@ -1,38 +1,51 @@
 module Admin
   class ApiSubmissionController < Admin::ApplicationController
-    attr_reader :api_json_endpoint_url, :api_json_endpoint_key, :service_id
+    attr_reader :endpoint_url, :endpoint_key, :service_id, :deployment_environment
 
     def initialize
-      @api_submission = ApiSubmission.new
+      settings = { service_id:, deployment_environment: }
+      @api_submission = Admin::ApiSubmission.new(settings)
+      super
+    end
+
+    def set_api_submission_configuration
+      @api_submission.service_id(params[:service_id])
+      @api_submission.endpoint_url(params[:admin_api_submission][:endpoint_url])
+      @api_submission.endpoint_key(params[:admin_api_submission][:endpoint_key])
     end
 
     def index
-      @service_id = params[:service_id]
+      Rails.logger.debug 'We index'
+      @api_submission.service_id(params[:service_id])
       render 'index'
     end
 
     def create
-      # create service configuration
-      @service_id = params[:service_id]
-      @api_json_endpoint_url = params[:api_json_endpoint_url]
-      @api_json_endpoint_url = params[:api_json_endpoint_key]
+      Rails.logger.debug 'We create'
+      set_api_submission_configuration
       # render 'admin/services/show', object: @service
+      @api_submission.create_service_configurations
       render 'index'
     end
 
-    def show
-      @service_id = params[:service_id]
-      @api_json_endpoint_url = 'saved value 1'
-      @api_json_endpoint_key = 'saved value 2'
-      # @api_json_endpoint_url = api_json_endpoint_url
-      # @api_json_endpoint_key = api_json_endpoint_key
-    end
+    # def show
+    #   Rails.logger.debug 'We show'
+    #   set_api_submission_configuration
+    #   @endpoint_url = 'saved value 1'
+    #   @endpoint_key = 'saved value 2'
+    # end
 
-    def update
-      @api_json_endpoint_url = params[:api_json_endpoint_url]
-      @api_json_endpoint_key = params[:api_json_endpoint_key]
-      # render 'admin/services/show', id: params[:service_id]
-      render 'admin/services/show'
+    # def update
+    #   Rails.logger.debug 'We update'
+    #   @endpoint_url = params[:endpoint_url]
+    #   @endpoint_key = params[:endpoint_key]
+    #   # render 'admin/services/show', id: params[:service_id]
+    #   render 'admin/services/show'
+    # end
+
+    def delete
+      Rails.logger.debug 'We have to delete'
+      # @api_submission.delete_service_configurations
     end
   end
 end
