d56e17c75d41881dae4f39dd4b2f4daa3189f6b0
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-04-25 15:55:58 +0100

    Adding model

---
+++ app/models/admin/api_submission.rb
@@ -0,0 +1,94 @@
+module Admin
+  class ApiSubmission
+    SERVICE_OUTPUT_JSON_ENDPOINT = 'SERVICE_OUTPUT_JSON_ENDPOINT'.freeze
+    SERVICE_OUTPUT_JSON_KEY = 'SERVICE_OUTPUT_JSON_KEY'.freeze
+    include ActiveModel::Model
+
+    validates :service_id, presence: true
+
+    def service_id(service_id)
+      @service_id = service_id
+    end
+
+    def endpoint_url(endpoint_url)
+      @endpoint_url = endpoint_url
+    end
+
+    def endpoint_key(endpoint_key)
+      @endpoint_key = endpoint_key
+    end
+
+    def initialize(settings)
+      @endpoint_url = saved_endpoint_url
+      @endpoint_key = saved_endpoint_key
+      @service_id = settings[:service_id]
+      # TODO: set in dev here like reference number
+      @deployment_environment = 'dev'
+    end
+
+    def saved_endpoint_url
+      @saved_endpoint_url ||=
+        begin
+          return service_config(name: SERVICE_OUTPUT_JSON_ENDPOINT).decrypt_value if service_config(name: SERVICE_OUTPUT_JSON_ENDPOINT).present?
+
+          'Not filled in'
+        end
+    end
+
+    def saved_endpoint_key
+      @saved_endpoint_key ||=
+        begin
+          return service_config(name: SERVICE_OUTPUT_JSON_KEY).decrypt_value if service_config(name: SERVICE_OUTPUT_JSON_KEY).present?
+
+          'Not filled in yet either'
+        end
+    end
+
+    def create_service_configurations
+      return if @service_id.blank?
+
+      create_or_update_configuration(
+        name: SERVICE_OUTPUT_JSON_ENDPOINT, value: @endpoint_url
+      )
+      create_or_update_configuration(
+        name: SERVICE_OUTPUT_JSON_KEY, value: @endpoint_key
+      )
+    end
+
+    def delete_service_configurations
+      return if @service_id.blank?
+
+      delete_service_configuration(name: SERVICE_OUTPUT_JSON_ENDPOINT)
+      delete_service_configuration(name: SERVICE_OUTPUT_JSON_KEY)
+    end
+
+    private
+
+    def service_config(name:)
+      return if @service_id.blank?
+
+      @service_config ||= ServiceConfiguration.find_by(
+        service_id: @service_id,
+        name:
+      )
+    end
+
+    def create_or_update_configuration(name:, value:)
+      service_configuration = ServiceConfiguration.find_or_initialize_by(
+        service_id: @service_id,
+        deployment_environment: @deployment_environment,
+        name:
+      )
+      service_configuration.value = value
+      service_configuration.save!
+    end
+
+    def delete_service_configuration(name:)
+      ServiceConfiguration.destroy_by(
+        service_id:,
+        deployment_environment: @deployment_environment,
+        name:
+      )
+    end
+  end
+end
