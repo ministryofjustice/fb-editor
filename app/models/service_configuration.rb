def91193031fe0059e3fe2d50377148760f5c330
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-04-26 10:38:41 +0100

    Put in secrets list to compare output

--- app/models/service_configuration.rb
+++ app/models/service_configuration.rb
@@ -4,6 +4,7 @@ class ServiceConfiguration < ApplicationRecord
     BASIC_AUTH_PASS
     ENCODED_PRIVATE_KEY
     SERVICE_SECRET
+    SAVED_FORMS_KEY
   ].freeze
   SUBMISSION = %w[
     SERVICE_EMAIL_OUTPUT
