583fe6c774999bae2a16f16f8fa5e5bdb1342081
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-04-26 10:50:52 +0100

    Checking if blank values are entered

--- app/controllers/admin/api_submission_controller.rb
+++ app/controllers/admin/api_submission_controller.rb
@@ -1,16 +1,10 @@
 module Admin
   class ApiSubmissionController < Admin::ApplicationController
-    attr_reader :endpoint_url, :endpoint_key, :service_id, :deployment_environment
+    attr_reader :service_id
 
     def initialize
       @api_submission = Admin::ApiSubmission.new
       super
-    end
-
-    def set_api_submission_configuration
-      @api_submission.service_id(params[:service_id])
-      @api_submission.endpoint_url(params[:admin_api_submission][:saved_endpoint_url])
-      @api_submission.endpoint_key(params[:admin_api_submission][:saved_endpoint_key])
     end
 
     def index
@@ -21,10 +15,18 @@ module Admin
     end
 
     def create
-      set_api_submission_configuration
+      url = params[:admin_api_submission][:saved_endpoint_url]
+      key = params[:admin_api_submission][:saved_endpoint_key]
+      @api_submission.service_id(params[:service_id])
+      @api_submission.endpoint_url(url)
+      @api_submission.endpoint_key(key)
+      if url.blank? || key.blank?
+        flash[:error] = 'Please use the delete button to delete API settings'
+      else
+        @api_submission.create_service_configurations
+        flash[:success] = 'API submission settings successfully updated'
+      end
       # render 'admin/services/show', object: @service
-      @api_submission.create_service_configurations
-      flash[:success] = "API submission settings successfully updated"
       render 'index'
     end
 
