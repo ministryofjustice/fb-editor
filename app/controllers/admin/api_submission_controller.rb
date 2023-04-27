635b5147c4781090221bd907691686e4cd31d81a
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-04-27 12:52:46 +0100

    Testing access config from the controller directly

--- app/controllers/admin/api_submission_controller.rb
+++ app/controllers/admin/api_submission_controller.rb
@@ -11,6 +11,7 @@ module Admin
       # we save the service field to be send as hidden field
       @service_id = params[:service_id]
       @api_submission.service_id(params[:service_id])
+      show
       render 'index'
     end
 
@@ -33,6 +34,65 @@ module Admin
       Rails.logger.debug 'We have to delete'
       @api_submission.delete_service_configurations
       render 'index'
+    end
+
+    SERVICE_OUTPUT_JSON_ENDPOINT = 'SERVICE_OUTPUT_JSON_ENDPOINT'.freeze
+    SERVICE_OUTPUT_JSON_KEY = 'SERVICE_OUTPUT_JSON_KEY'.freeze
+
+    def show
+      puts 'We show'
+      @url_dev = url_dev
+      @url_prod = url_prod
+      @key_dev = key_dev
+      @key_prod = key_prod
+    end
+
+    def url_dev
+      @service_id = params[:service_id]
+      config = service_config(service_id: @service_id, deployment_environment: 'dev', name: SERVICE_OUTPUT_JSON_ENDPOINT)
+      @url_dev ||=
+        begin
+          return config.decrypt_value if config.present?
+
+          'default1'
+        end
+    end
+
+    def key_dev
+      @service_id = params[:service_id]
+      config = service_config(service_id: @service_id, deployment_environment: 'dev', name: SERVICE_OUTPUT_JSON_KEY)
+      @key_dev ||=
+        begin
+          return config.decrypt_value if config.present?
+
+          'default2'
+        end
+    end
+
+    def url_prod
+      @service_id = params[:service_id]
+      config = service_config(service_id: @service_id, deployment_environment: 'production', name: SERVICE_OUTPUT_JSON_ENDPOINT)
+      @url_prod ||=
+        begin
+          return config.decrypt_value if config.present?
+
+          'default3'
+        end
+    end
+
+    def key_prod
+      @service_id = params[:service_id]
+      config = service_config(service_id: @service_id, deployment_environment: 'production', name: SERVICE_OUTPUT_JSON_KEY)
+      @key_prod ||=
+        begin
+          return config.decrypt_value if config.present?
+
+          'default4'
+        end
+    end
+
+    def service_config(service_id:, name:, deployment_environment:)
+      @service_config ||= ServiceConfiguration.find_by(service_id:, deployment_environment:, name:)
     end
   end
 end
