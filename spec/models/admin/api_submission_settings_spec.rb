02be65740c42cc240baab4a0621802df4f1eda1f
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-05-09 12:11:52 +0100

    Refactoring tests because of false positive

--- spec/models/admin/api_submission_settings_spec.rb
+++ spec/models/admin/api_submission_settings_spec.rb
@@ -21,25 +21,20 @@ RSpec.describe Admin::ApiSubmissionSettings, type: :model do
 
     context 'service_output_json_key' do
       let(:deployment_environment) { 'dev' }
-      let(:service_output_json_endpoint) { 'valid .gov.uk' }
-      let(:params) { { deployment_environment:, service_output_json_endpoint: } }
+      let(:service_output_json_endpoint) { 'http://this-valid-url.com' }
+      let(:params) { { deployment_environment:, service_output_json_endpoint:, service_output_json_key: } }
+      let(:expected_error) { described_class::KEY_LENGTH_ERROR }
 
-      it 'has to be 16 characters long' do
-        should allow_values('98957af1a0424376', 'f89ea3f29cbdece2').for(:service_output_json_key)
+      context 'has to be 16 characters long' do
+        let(:service_output_json_key) { '98957af1a0424376' }
+
+        it 'is valid' do
+          expect(subject).to be_valid
+        end
       end
 
-      # it 'do not allow anything else' do
-      #   should_not allow_values(
-      #     nil, 'abc', 'jdsiojd89ofi', 'c8c6f4710e6ebf28f89ea3f29cbdece2'
-      #   ).for(:service_output_json_key)
-      # end
-
       context 'key lenght is too short' do
-        let(:deployment_environment) { 'dev' }
-        let(:service_output_json_endpoint) { 'valid .gov.uk' }
         let(:service_output_json_key) { 'abc' }
-        let(:params) { { deployment_environment:, service_output_json_endpoint:, service_output_json_key: } }
-        let(:expected_error) { described_class::KEY_LENGTH_ERROR }
 
         it 'setting should be invalid' do
           expect(subject).to_not be_valid
@@ -52,10 +47,7 @@ RSpec.describe Admin::ApiSubmissionSettings, type: :model do
       end
 
       context 'key lenght is too long' do
-        let(:deployment_environment) { 'dev' }
-        let(:service_output_json_endpoint) { 'valid .gov.uk' }
         let(:service_output_json_key) { 'c8c6f4710e6ebf28f89ea3f29cbdece2' }
-        let(:params) { { deployment_environment:, service_output_json_endpoint:, service_output_json_key: } }
         let(:expected_error) { described_class::KEY_LENGTH_ERROR }
 
         it 'setting should be invalid' do
@@ -72,19 +64,32 @@ RSpec.describe Admin::ApiSubmissionSettings, type: :model do
     context 'service_output_json_endpoint' do
       let(:deployment_environment) { 'production' }
       let(:service_output_json_key) { 'f89ea3f29cbdece2' }
-      let(:params) { { deployment_environment:, service_output_json_key: } }
+      let(:params) { { deployment_environment:, service_output_json_endpoint:, service_output_json_key: } }
 
-      it 'has to be a url' do
-        should allow_values(
-          'http://this-valid-url.com', 'valid.gov.uk'
-        ).for(:service_output_json_endpoint)
+      context 'it is a correct url' do
+        let(:service_output_json_endpoint) { 'http://this-valid-url.com' }
+
+        it 'setting should be valid' do
+          expect(subject).to be_valid
+        end
       end
 
-      # it 'do not allow anything else' do
-      #   should_not allow_values(
-      #     nil, 'abc', 'not a valid url'
-      #   ).for(:service_output_json_endpoint)
-      # end
+      context 'url field doesn\'t have a scheme prefix' do
+        let(:deployment_environment) { 'dev' }
+        let(:service_output_json_key) { 'f89ea3f29cbdece2' }
+        let(:service_output_json_endpoint) { 'valid.gov.uk' }
+        let(:params) { { deployment_environment:, service_output_json_endpoint:, service_output_json_key: } }
+        let(:expected_error) { described_class::URL_ERROR }
+
+        it 'setting should be invalid' do
+          expect(subject).to_not be_valid
+        end
+
+        it 'should have errors' do
+          subject.valid?
+          expect(subject.errors.first.type).to eq(expected_error)
+        end
+      end
     end
   end
 end
