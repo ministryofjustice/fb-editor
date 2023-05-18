2c3e6aac1fda115b4bcdbd2950ae35ea2deaf7d2
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-05-18 14:21:13 +0100

    Api submission (#2203)
    * Updating key lenght to 17 instead of 16
    
    * Removing validation of the endpoint key length
    
    * Addressing comments from review
    
    * Adding tests for presence validation in api submission settings

--- spec/models/admin/api_submission_settings_spec.rb
+++ spec/models/admin/api_submission_settings_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Admin::ApiSubmissionSettings, type: :model do
       described_class.new(params.merge(service:))
     end
     let(:params) { {} }
+    let(:service_output_json_key) { 'f89ea3f29cbdece2x' }
 
     context 'deployment environment' do
-      let(:service_output_json_key) { 'f89ea3f29cbdece2' }
       let(:service_output_json_endpoint) { 'valid.gov.uk' }
       let(:params) { { service_output_json_key:, service_output_json_endpoint: } }
 
@@ -19,58 +19,8 @@ RSpec.describe Admin::ApiSubmissionSettings, type: :model do
       end
     end
 
-    context 'service_output_json_key' do
-      let(:deployment_environment) { 'dev' }
-      let(:service_output_json_endpoint) { 'http://this-valid-url.com' }
-      let(:params) { { deployment_environment:, service_output_json_endpoint:, service_output_json_key: } }
-      let(:expected_error) { described_class::KEY_LENGTH_ERROR }
-
-      context 'has to be 16 characters long' do
-        let(:service_output_json_key) { '98957af1a0424376' }
-
-        it 'is valid' do
-          expect(subject).to be_valid
-        end
-      end
-
-      context 'is empty to reset configuration' do
-        let(:service_output_json_key) { '' }
-
-        it 'is valid' do
-          expect(subject).to be_valid
-        end
-      end
-
-      context 'key length is too short' do
-        let(:service_output_json_key) { 'abc' }
-
-        it 'setting should be invalid' do
-          expect(subject).to_not be_valid
-        end
-
-        it 'should have errors' do
-          subject.valid?
-          expect(subject.errors.first.type).to eq(expected_error)
-        end
-      end
-
-      context 'key length is too long' do
-        let(:service_output_json_key) { 'c8c6f4710e6ebf28f89ea3f29cbdece2' }
-
-        it 'setting should be invalid' do
-          expect(subject).to_not be_valid
-        end
-
-        it 'should have errors' do
-          subject.valid?
-          expect(subject.errors.first.type).to eq(expected_error)
-        end
-      end
-    end
-
     context 'service_output_json_endpoint' do
       let(:deployment_environment) { 'production' }
-      let(:service_output_json_key) { 'f89ea3f29cbdece2' }
       let(:params) { { deployment_environment:, service_output_json_endpoint:, service_output_json_key: } }
       let(:expected_error) { described_class::URL_ERROR }
 
@@ -101,6 +51,18 @@ RSpec.describe Admin::ApiSubmissionSettings, type: :model do
           subject.valid?
           expect(subject.errors.first.type).to eq(expected_error)
         end
+      end
+    end
+
+    context 'service_output_json_key' do
+      let(:service_output_json_key) { nil }
+
+      it 'do not allow nil' do
+        expect(subject).to_not be_valid
+      end
+
+      it 'allow blank' do
+        should allow_values('').for(:service_output_json_key)
       end
     end
   end
