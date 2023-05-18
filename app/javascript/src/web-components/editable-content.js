07000380bab689c6964591189914313d33e1fea4
Chris Pymm <chris@chrispymm.co.uk>
2023-05-18 15:30:09 +0100

    refactor

--- app/javascript/src/web-components/editable-content.js
+++ app/javascript/src/web-components/editable-content.js
@@ -5,7 +5,7 @@ import GovukHTMLRenderer from 'govuk-markdown'
 class EditableContent extends HTMLElement {
   constructor() {
     super() 
-   
+
     const self = this
 
     this.state = new Proxy(
@@ -32,36 +32,52 @@ class EditableContent extends HTMLElement {
   }
 
   connectedCallback() {
+    // connectedCallback is fired as soon as the opening element is parsed
+    // setTimeount allows the child elements to be parsed before we try to read
+    // the initialMarkup
     setTimeout(() => {
       this.initialMarkup = this.innerHTML;
+      this.initialContent = this.getAttribute('content').replace(/\\r\\n?|\\n/g, '\n');
+      this.defaultContent = this.getAttribute('default-content');
+      this.json = (this.dataset.json ? JSON.parse(this.dataset.json) : undefined);
       this.render();
     })
   }
 
-  get initialContent() {
-    return this.getAttribute('content').replace(/\\r\\n?|\\n/g, '\n');
-  }
-
-  get defaultContent() {
-    return this.getAttribute('default-content');
-  }
-
+  // Markdown content that will be saved
+  // If content area is part of page components we need to return compontn json
+  // as a string otherwise we return the content string
   get content() {
-    return this.input.value == this.defaultContent ? '' : this.input.value;
+    console.log('getting content')
+    if(this.isComponent) {
+      console.log('this is a component')
+      this.json.content = this.input.value;
+      console.log(this)
+      return JSON.stringify(this.json);
+    } else {
+      return this.input.value == this.defaultContent ? '' : this.input.value;
+    }
   }
 
+  // Is the content area part of the page components 
+  get isComponent() {
+    return !!this.json;
+  }
+
+  // The form containing the element that will be updated on save
   set form(element) {
     this.submissionForm = element;
   }
 
   render() {
     const initialContent = this.initialContent || this.defaultContent;
+    const initialMarkup = (this.initialMarkup.trim() != '' ? this.initialMarkup : this.defaultContent);
     this.innerHTML = `
     <div class="editable-content" data-element="editable-content-root">
       <textarea class="govuk-textarea" data-element="editable-content-input" rows="10" hidden>${initialContent}</textarea>
-      <div data-element="editable-content-output">${this.initialMarkup}</div>
+      <div data-element="editable-content-output">${initialMarkup}</div>
     </div>`
-    
+
     this.afterRender()
   }
 
@@ -74,7 +90,7 @@ class EditableContent extends HTMLElement {
       this.root.setAttribute('tabindex', '0');
 
       this.input.dataset.minRows = this.input.rows || 5;
-      
+
       this.root.addEventListener('click', () => { this.state.mode === 'read' ? this.state.mode = 'edit' : '' });
       this.root.addEventListener('focus', () => { this.state.mode === 'read' ? this.state.mode = 'edit' : '' });
       this.input.addEventListener('blur', () => { this.state.mode === 'edit' ? this.state.mode = 'read' : '' });
@@ -86,10 +102,11 @@ class EditableContent extends HTMLElement {
 
       return
     }
-
+    
+    // If we don't have input and output elements, bail and just restore the
+    // initial markup
     this.innerHTML = this.initialMarkup;
   }
-
 
   processStateChange() {
     this.root.setAttribute('mode', this.state.mode)
@@ -97,25 +114,25 @@ class EditableContent extends HTMLElement {
     switch(this.state.mode) {
       case 'edit':
         this._contentBeforeEditing = this.input.value.trim();
-        this.input.removeAttribute('hidden')
-        this.output.setAttribute('hidden', '')
+        this.show(this.input)
+        this.hide(this.output)
         this.updateHeight(this.input);
-        this.input.focus()
-        if(this.input.value == this.defaultContent) {
-          this.input.value = '';
-        }
+        this.input.focus();
+
+        if(this.valueIsDefault()) this.input.value = '';
+        // Move cursor to end of content 
         this.input.selectionStart = this.input.value.length;
         break;
       case 'read':
       case 'initial':
-        if(this.input.value.trim() == '') {
-          this.input.value = this.defaultContent
-        }
-        this.renderContent();
+        if(this.valueIsDefault()) this.input.value = '';
         
-        this.input.setAttribute('hidden', '')
-        this.output.removeAttribute('hidden')
-        if(this.input.value.trim() !== this._contentBeforeEditing ) {
+        this.updateOutput();
+        this.hide(this.input);
+        this.show(this.output);
+
+        if(this.valueHasChanged()) {
+          this.save();
           document.dispatchEvent( 
             new CustomEvent( 
               'SaveRequired', 
@@ -129,7 +146,15 @@ class EditableContent extends HTMLElement {
     }
   }
 
-  renderContent(){
+  hide(element) {
+    element.setAttribute('hidden', '');
+  }
+
+  show(element) {
+    element.removeAttribute('hidden');
+  }
+
+  updateOutput(){
     this.output.innerHTML = marked.parse(this.input.value);
   }
 
@@ -141,72 +166,81 @@ class EditableContent extends HTMLElement {
     }
   }
 
+  valueIsDefault() {
+    return this.input.value == this.defaultContent;
+  }
+
+  valueHasChanged() {
+    return this.input.value.trim() !== this._contentBeforeEditing 
+  }
+
   isScrolling(textareaEl) {
     return textareaEl.scrollHeight > textareaEl.clientHeight;
   }
 
   rows(textareaEl) {
-      return textareaEl.rows;
+    return textareaEl.rows;
   }
 
   grow(textareaEl) {
-      // Store initial height of textarea
-      let previousHeight = textareaEl.clientHeight;
-      let rows = this.rows(textareaEl);
+    // Store initial height of textarea
+    let previousHeight = textareaEl.clientHeight;
+    let rows = this.rows(textareaEl);
 
-      while (this.isScrolling(textareaEl)) {
-        rows++;
-        textareaEl.rows = rows;
+    while (this.isScrolling(textareaEl)) {
+      rows++;
+      textareaEl.rows = rows;
 
-        // Get height after rows change is made
-        const newHeight = textareaEl.clientHeight ;
+      // Get height after rows change is made
+      const newHeight = textareaEl.clientHeight ;
 
-        // If the height hasn't changed, break the loop
-        // This is an important safety check in case the height is hard coded
-        if (newHeight === previousHeight) break;
+      // If the height hasn't changed, break the loop
+      // This is an important safety check in case the height is hard coded
+      if (newHeight === previousHeight) break;
 
-        // Store the updated height for the next comparison and proceed
-        previousHeight = newHeight;
+      // Store the updated height for the next comparison and proceed
+      previousHeight = newHeight;
+    }
+  }
+
+  /** Shrink until the textarea matches the minimum rows or starts overflowing */
+  shrink(textareaEl) {
+    // Store initial height of textarea
+    let previousHeight = textareaEl.clientHeight;
+
+    const minRows = parseInt(textareaEl.dataset.minRows);
+    let rows = this.rows(textareaEl);
+
+    while (!this.isScrolling(textareaEl) && rows > minRows) {
+      rows--;
+      textareaEl.rows = Math.max(rows, minRows);
+
+      // Get height after rows change is made
+      const newHeight = textareaEl.clientHeight;
+
+      // If the height hasn't changed, break the loop
+      // This is an important safety check in case the height is hard coded
+      if (newHeight === previousHeight) break;
+
+      // If we shrunk so far that we're overflowing, add a row back on.
+      if (this.isScrolling(textareaEl)) {
+        this.grow(textareaEl);
+        break;
       }
     }
-
-    /** Shrink until the textarea matches the minimum rows or starts overflowing */
-    shrink(textareaEl) {
-      // Store initial height of textarea
-      let previousHeight = textareaEl.clientHeight;
-
-      const minRows = parseInt(textareaEl.dataset.minRows);
-      let rows = this.rows(textareaEl);
-
-      while (!this.isScrolling(textareaEl) && rows > minRows) {
-        rows--;
-        textareaEl.rows = Math.max(rows, minRows);
-
-        // Get height after rows change is made
-        const newHeight = textareaEl.clientHeight;
-
-        // If the height hasn't changed, break the loop
-        // This is an important safety check in case the height is hard coded
-        if (newHeight === previousHeight) break;
-
-        // If we shrunk so far that we're overflowing, add a row back on.
-        if (this.isScrolling(textareaEl)) {
-          this.grow(textareaEl);
-          break;
-        }
-      }
-    }
+  }
 
   save() {
     if(this.submissionForm) {
       const hiddenInput = this.submissionForm.querySelector(`input[name="${this.id}"]`);
       if(hiddenInput) {
-        hiddenInput.value = this.input.value;
+        hiddenInput.value = this.content;
       } else {
         const button = this.submissionForm.querySelector('button[type="submit], input[type="submit"]');
         const newHiddenInput = document.createElement('input');
         newHiddenInput.setAttribute('name', this.id);
         newHiddenInput.setAttribute('type', 'hidden');
+        hiddenInput.value = this.content;
         button.insertAdjacentHTML('beforebegin', newHiddenInput);
       }
     }
