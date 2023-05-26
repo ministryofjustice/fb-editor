a18b1bff287262f622002818c35ea44d4c00ea4a
Natalie Seeto <njseeto@users.noreply.github.com>
2023-05-26 15:20:27 +0100

    Create PREVIOUS_SERVICE_SLUG if form has been published
    We don't want to overwrite the existing service slug config if
    the form has been published as we will need this to unpublish any
    test forms.
    
    We want to create a PREVIOUS_SERVICE_SLUG if:
    - a form has been published
    - there is no exisiting PREVIOUS_SERVICE_SLUG record.
    
    This is because we do not want to overwrite the PREVIOUS_SERVICE_SLUG
    config for every update.

--- app/models/form_name_updater.rb
+++ app/models/form_name_updater.rb
@@ -8,18 +8,28 @@ class FormNameUpdater
 
   def create_or_update!
     ActiveRecord::Base.transaction do
-      save_config_service_slug
+      if previous_service_slug.nil? && currently_published?
+        save_config_previous_service_slug
+        save_config_service_slug
+      else
+        save_config_service_slug
+      end
     end
   end
 
   private
 
   def save_config_service_slug
-    create_or_update_service_configuration(config: 'SERVICE_SLUG', deployment_environment: 'dev')
-    create_or_update_service_configuration(config: 'SERVICE_SLUG', deployment_environment: 'production')
+    create_or_update_service_configuration(config: 'SERVICE_SLUG', deployment_environment: 'dev', value: service_slug)
+    create_or_update_service_configuration(config: 'SERVICE_SLUG', deployment_environment: 'production', value: service_slug)
   end
 
-  def create_or_update_service_configuration(config:, deployment_environment:, value: service_slug)
+  def save_config_previous_service_slug
+    create_or_update_service_configuration(config: 'PREVIOUS_SERVICE_SLUG', deployment_environment: 'dev', value: existing_service_slug_config)
+    create_or_update_service_configuration(config: 'PREVIOUS_SERVICE_SLUG', deployment_environment: 'production', value: existing_service_slug_config)
+  end
+
+  def create_or_update_service_configuration(config:, deployment_environment:, value:)
     setting = find_or_initialize_setting(config, deployment_environment)
     setting.value = value
     setting.save!
@@ -52,14 +62,35 @@ class FormNameUpdater
   end
 
   def all_service_slugs
-    @all_service_slugs ||= existing_service_slugs.concat(previous_service_slugs)
+    @all_service_slugs ||= all_existing_service_slugs.concat(all_previous_service_slugs)
   end
 
-  def existing_service_slugs
-    @existing_service_slugs ||= ServiceConfiguration.where(name: 'SERVICE_SLUG').map(&:decrypt_value)
+  def all_existing_service_slugs
+    @all_existing_service_slugs ||= ServiceConfiguration.where(name: 'SERVICE_SLUG').map(&:decrypt_value)
   end
 
-  def previous_service_slugs
-    @previous_service_slugs ||= ServiceConfiguration.where(name: 'PREVIOUS_SERVICE_SLUG').map(&:decrypt_value)
+  def all_previous_service_slugs
+    @all_previous_service_slugs ||= ServiceConfiguration.where(name: 'PREVIOUS_SERVICE_SLUG').map(&:decrypt_value)
+  end
+
+  def existing_service_slug_config
+    @existing_service_slug_config ||= ServiceConfiguration.find_by(
+      service_id: service_id,
+      name: 'SERVICE_SLUG'
+    )&.decrypt_value
+  end
+
+  def previous_service_slug
+    @previous_service_slug ||= ServiceConfiguration.find_by(
+      service_id: service_id,
+      name: 'PREVIOUS_SERVICE_SLUG'
+    )
+  end
+
+  def currently_published?
+    PublishService.where(
+      service_id:,
+      deployment_environment: 'dev'
+    ).last&.published?
   end
 end
--- spec/models/form_name_updater_spec.rb
+++ spec/models/form_name_updater_spec.rb
@@ -12,6 +12,12 @@ RSpec.describe FormNameUpdater do
       name: 'SERVICE_SLUG'
     )
   end
+  let(:previous_slug_service_config) do
+    ServiceConfiguration.find_by(
+      service_id: service.service_id,
+      name: 'PREVIOUS_SERVICE_SLUG'
+    )
+  end
 
   describe '#create_or_update' do
     context 'on form creation' do
@@ -127,6 +133,72 @@ RSpec.describe FormNameUpdater do
           expect(
             service_configuration.decrypt_value
           ).to eq(expected_service_slug)
+        end
+      end
+
+      context 'when service has been published' do
+        let(:attributes) { { service_name: 'I am a unique service' } }
+        let(:expected_service_slug) { 'i-am-a-unique-service' }
+        let(:previous_service_slug) { 'eat-slugs-malfoy' }
+
+        before do
+          create(
+            :service_configuration,
+            :service_slug,
+            service_id: service.service_id,
+            deployment_environment: 'dev'
+          )
+          create(
+            :service_configuration,
+            :service_slug,
+            service_id: service.service_id,
+            deployment_environment: 'production'
+          )
+
+          allow_any_instance_of(FormNameUpdater).to receive(:currently_published?).and_return(true)
+        end
+
+        context 'when previous service slug config does not exist' do
+          it 'creates a previous service slug config and updates service slug config' do
+            form_name_updater.create_or_update!
+            service_configuration.reload
+            expect(
+              previous_slug_service_config.decrypt_value
+            ).to eq(previous_service_slug)
+            expect(
+              service_configuration.decrypt_value
+            ).to eq(expected_service_slug)
+          end
+        end
+
+        context 'when previous service slug exists' do
+          let(:previous_service_slug) { 'slug-life' }
+
+          before do
+            create(
+              :service_configuration,
+              :previous_service_slug,
+              service_id: service.service_id,
+              deployment_environment: 'dev'
+            )
+            create(
+              :service_configuration,
+              :previous_service_slug,
+              service_id: service.service_id,
+              deployment_environment: 'production'
+            )
+          end
+
+          it 'updates service slug config only' do
+            form_name_updater.create_or_update!
+            service_configuration.reload
+            expect(
+              previous_slug_service_config.decrypt_value
+            ).to eq(previous_service_slug)
+            expect(
+              service_configuration.decrypt_value
+            ).to eq(expected_service_slug)
+          end
         end
       end
     end
