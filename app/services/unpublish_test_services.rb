2f4baac02c1f7f7e7590d5aa1900012152a679da
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-05-30 12:00:23 +0100

    Adding API submission to acceptance tests service list (#2225)

--- app/services/unpublish_test_services.rb
+++ app/services/unpublish_test_services.rb
@@ -4,7 +4,8 @@ class UnpublishTestServices
   ACCEPTANCE_TEST_SERVICES = [
     'cd75ad76-1d4b-4ce5-8a9e-035262cd2683', # New Runner Service
     'e68dca75-20b8-468e-9436-e97791a914c5', # Branching Fixture 10 Service
-    '759716eb-b4fb-413e-b883-f7016e2a9feb'  # Save and Return v2 Service
+    '759716eb-b4fb-413e-b883-f7016e2a9feb', # Save and Return v2 Service
+    '1ef15479-5a2c-4426-a5bf-54253031d9be'  # API Submission JSON Output v2 Service
   ].freeze
   ENVIRONMENTS = %i[dev production].freeze
   SELECT_LATEST_PUBLISH_SERVICE_RECORD = 'SELECT DISTINCT ON (service_id) * FROM publish_services ORDER BY service_id, created_at DESC'.freeze
