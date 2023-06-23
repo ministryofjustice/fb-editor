45ce426a6abdd2790aea9e7582e1d02047ff739d
Natalie Seeto <njseeto@users.noreply.github.com>
2023-06-23 14:47:06 +0100

    Remove memoisation in Submission Pages Presenter
    This doesn't fix the grid performance issue so changing it back
    to what it was.

--- app/presenters/submission_pages_presenter.rb
+++ app/presenters/submission_pages_presenter.rb
@@ -33,27 +33,27 @@ class SubmissionPagesPresenter
   end
 
   def cya_and_confirmation_missing?
-    !checkanswers_in_main_flow &&
-      !confirmation_in_main_flow
+    !checkanswers_in_main_flow? &&
+      !confirmation_in_main_flow?
   end
 
   def cya_page_not_present_message
-    if !checkanswers_in_main_flow && confirmation_in_main_flow
+    if !checkanswers_in_main_flow? && confirmation_in_main_flow?
       messages[:cya_page].gsub('%{href}', link('cya_page')).html_safe
     end
   end
 
   def confirmation_page_not_present_message
-    if checkanswers_in_main_flow && !confirmation_in_main_flow
+    if checkanswers_in_main_flow? && !confirmation_in_main_flow?
       messages[:confirmation_page].gsub('%{href}', link('confirmation_page')).html_safe
     end
   end
 
-  def checkanswers_in_main_flow
-    @checkanswers_in_main_flow ||= @grid.page_uuids.include?(service.checkanswers_page&.uuid)
+  def checkanswers_in_main_flow?
+    @grid.page_uuids.include?(service.checkanswers_page&.uuid)
   end
 
-  def confirmation_in_main_flow
-    @confirmation_in_main_flow ||= @grid.page_uuids.include?(service.confirmation_page&.uuid)
+  def confirmation_in_main_flow?
+    @grid.page_uuids.include?(service.confirmation_page&.uuid)
   end
 end
