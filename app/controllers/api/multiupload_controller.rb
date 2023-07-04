1de075ed88073549b9f9373cf31a1298831e1fcc
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-07-04 13:53:53 +0100

    Cleanup

--- app/controllers/api/multiupload_controller.rb
+++ app/controllers/api/multiupload_controller.rb
@@ -17,23 +17,18 @@ module Api
             anchor: @metadata_updater.component_added.try(:id)
           ) and return
         else
-          # @page.errors.add(:base, :unprocessable)
           render :edit, status: :unprocessable_entity and return
         end
       end
       redirect_to api_service_multiupload_path(service.service_id, params['page_id'], params['component_id']), status: :bad_request
-      # render partial: 'show', layout: false, status: :bad_request, locals: { 'page_id' => params['page_id'], 'component_id' => params['component_id'] } and return
     end
 
-    def has_error
-      # byebug
-    end
+    def has_error; end
 
     def update_params
-      # byebug
       page = service.pages.select { |p| p._uuid == params['page_id'] }.first.deep_dup
       page.metadata.components.select { |c| c['_uuid'] == params['component_id'] }.first['max_files'] = params['max_files_value']
-      # page.component['max_files'] = params['max_files_value']
+
       ActiveSupport::HashWithIndifferentAccess.new({
         uuid: params['component']['page_uuid'],
         latest_metadata: service_metadata,
