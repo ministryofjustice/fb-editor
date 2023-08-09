03fc058041d3cb92d26075d4bc1f92ee154dcac3
Natalie Seeto <njseeto@users.noreply.github.com>
2023-08-09 11:07:07 +0100

    ComponentExpression model
    This model ensures we return the correct expressions and expression
    answers, when a question is selected on the conditional content
    modal.
    
    For now, we only support radios and checkboxes in conditional
    content, and these are the only questions that should return
    expressions and expression answers.
    
    Since this is similar to branching, we have re-used the
    `supports_branching?` method, to check the component type is either
    radio or checkbox.
    
    We also have different operators depending on whether it is radio
    or checkbox - this is addressed in the `operators` method.
    
    We also do not return any expression answers if the chosen operator
    is `answered` or `not_answered`.
    
    This class is largely similar to the `Expression` model used for
    branching - we can look at refactoring once we have the feature
    in place.

---
+++ app/models/component_expression.rb
@@ -0,0 +1,101 @@
+class ComponentExpression
+  include ActiveModel::Model
+  attr_accessor :component, :operator, :field, :page
+
+  validates :component, :operator, :page, presence: true
+  validate :unsupported_component, :blank_field
+
+  OPERATORS = [
+    [I18n.t('operators.is'), 'is', { 'data-hide-answers': 'false' }],
+    [I18n.t('operators.is_not'), 'is_not', { 'data-hide-answers': 'false' }],
+    [I18n.t('operators.contains'), 'contains', { 'data-hide-answers': 'false' }],
+    [I18n.t('operators.does_not_contain'), 'does_not_contain', { 'data-hide-answers': 'false' }],
+    [I18n.t('operators.is_answered'), 'is_answered', { 'data-hide-answers': 'true' }],
+    [I18n.t('operators.is_not_answered'), 'is_not_answered', { 'data-hide-answers': 'true' }]
+  ].freeze
+
+  def to_metadata
+    {
+      'operator' => operator,
+      'page' => page&.uuid,
+      'component' => component,
+      'field' => field_answer
+    }
+  end
+
+  def answers
+    component_object.items.map { |item| [item.label, item.uuid] }
+  end
+
+  def all_operators
+    OPERATORS
+  end
+
+  def operators
+    return contains_operators if is_checkbox?
+
+    is_operators
+  end
+
+  def component_type
+    component_object.type
+  end
+
+  def name_attr(conditional_index:, expression_index:, attribute:)
+    "conditional_component[conditionals_attributes][#{conditional_index}]" \
+    "[expressions_attributes][#{expression_index}][#{attribute}]"
+  end
+
+  def id_attr(conditional_index:, expression_index:, attribute:)
+    "conditional_component_conditionals_attributes_#{conditional_index}_" \
+    "expressions_attributes_#{expression_index}_#{attribute}"
+  end
+
+  private
+
+  def field_answer
+    return field.to_s unless field_required?
+
+    field
+  end
+
+  def field_required?
+    operator == I18n.t('operators.is') || operator == I18n.t('operators.is_not')
+  end
+
+  def component_object
+    @component_object ||= page.find_component_by_uuid(component)
+  end
+
+  def contains_operators
+    @contains_operators ||= all_operators.select do |operator|
+      operator.exclude?(I18n.t('operators.is')) && operator.exclude?(I18n.t('operators.is_not'))
+    end
+  end
+
+  def is_operators
+    @is_operators ||= all_operators.select do |operator|
+      operator.exclude?(I18n.t('operators.contains')) && operator.exclude?(I18n.t('operators.does_not_contain'))
+    end
+  end
+
+  def is_checkbox?
+    component_type == 'checkboxes'
+  end
+
+  def unsupported_component
+    if page.present? && component.present? && !component_object.supports_branching?
+      errors.add(:component, message: I18n.t(
+        'activemodel.errors.messages.unsupported_component'
+      ))
+    end
+  end
+
+  def blank_field
+    if field_required? && field.nil?
+      errors.add(:operator, message: I18n.t(
+        'activemodel.errors.messages.blank'
+      ))
+    end
+  end
+end
