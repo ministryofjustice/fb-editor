389669ec1b988a368344610089c982d29bd6d44c
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-09-11 12:22:36 +0100

    Lint

--- spec/services/publisher/adapters/aws_s3_client_spec.rb
+++ spec/services/publisher/adapters/aws_s3_client_spec.rb
@@ -12,23 +12,25 @@ RSpec.describe Publisher::Adapters::AwsS3Client do
 
     it 'should upload an object to s3' do
       # stub the token requests that the pod service account makes
-      stub_request(:put, "http://169.254.169.254/latest/api/token").
-      with(
+      stub_request(:put, 'http://169.254.169.254/latest/api/token')
+      .with(
         headers: {
-       'Accept'=>'*/*',
-       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       'User-Agent'=>'aws-sdk-ruby3/3.181.0',
-       'X-Aws-Ec2-Metadata-Token-Ttl-Seconds'=>'21600'
-        }).
-      to_return(status: 200, body: "", headers: {})
-      stub_request(:get, "http://169.254.169.254/latest/meta-data/iam/security-credentials/").
-      with(
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent' => 'aws-sdk-ruby3/3.181.0',
+          'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '21600'
+        }
+      )
+      .to_return(status: 200, body: '', headers: {})
+      stub_request(:get, 'http://169.254.169.254/latest/meta-data/iam/security-credentials/')
+      .with(
         headers: {
-       'Accept'=>'*/*',
-       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       'User-Agent'=>'aws-sdk-ruby3/3.181.0'
-        }).
-      to_return(status: 200, body: "", headers: {})
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent' => 'aws-sdk-ruby3/3.181.0'
+        }
+      )
+      .to_return(status: 200, body: '', headers: {})
 
       expect_any_instance_of(Aws::S3::Client).to receive(:put_object).with(
         body:,
