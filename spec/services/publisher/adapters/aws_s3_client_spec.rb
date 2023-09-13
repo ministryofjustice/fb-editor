52e0048ed37290d9cbd07458145aa1bacdc66806
StevenLeighton21 <steven.leighton@digital.justice.gov.uk>
2023-09-13 14:28:33 +0100

    Revert "Rotate to short lived S3 credentials (#2404)"
    This reverts commit 40eed3d8d6be684e0559af2e432349669c5aa6ce.

--- spec/services/publisher/adapters/aws_s3_client_spec.rb
+++ spec/services/publisher/adapters/aws_s3_client_spec.rb
@@ -1,7 +1,9 @@
 RSpec.describe Publisher::Adapters::AwsS3Client do
   subject(:aws_s3_client) do
     described_class.new(
-      bucket:
+      bucket:,
+      access_key_id: 'some-access-key_id',
+      secret_access_key: 'some-secret-access-key'
     )
   end
 
@@ -11,27 +13,6 @@ RSpec.describe Publisher::Adapters::AwsS3Client do
     let(:object_key) { 'some-object-key' }
 
     it 'should upload an object to s3' do
-      # stub the token requests that the pod service account makes
-      stub_request(:put, 'http://169.254.169.254/latest/api/token')
-      .with(
-        headers: {
-          'Accept' => '*/*',
-          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent' => 'aws-sdk-ruby3/3.181.0',
-          'X-Aws-Ec2-Metadata-Token-Ttl-Seconds' => '21600'
-        }
-      )
-      .to_return(status: 200, body: '', headers: {})
-      stub_request(:get, 'http://169.254.169.254/latest/meta-data/iam/security-credentials/')
-      .with(
-        headers: {
-          'Accept' => '*/*',
-          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent' => 'aws-sdk-ruby3/3.181.0'
-        }
-      )
-      .to_return(status: 200, body: '', headers: {})
-
       expect_any_instance_of(Aws::S3::Client).to receive(:put_object).with(
         body:,
         bucket:,
