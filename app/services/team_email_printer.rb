338b0fe9a4f355ef865f0bc4198eff642ed26211
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-09-26 15:51:52 +0100

    need that

--- app/services/team_email_printer.rb
+++ app/services/team_email_printer.rb
@@ -8,4 +8,8 @@ class TeamEmailPrinter
   def user_ids
     User.all.map { |user| user.id if user.email.in?(team_emails) }.compact
   end
+
+  def team_emails
+    @team_emails ||= Rails.application.config.moj_forms_team
+  end
 end
