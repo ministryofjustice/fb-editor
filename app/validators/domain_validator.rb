4d9e30fd915d29021fa0a5c44744534971779a55
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-10-24 14:57:05 +0100

    Allow case insensitive domains for service_email_output

--- app/validators/domain_validator.rb
+++ app/validators/domain_validator.rb
@@ -1,7 +1,7 @@
 class DomainValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     domain = value.split('@').last
-    if value.present? && !domain.in?(Rails.application.config.allowed_domains)
+    if value.present? && !domain.downcase.in?(Rails.application.config.allowed_domains)
       record.errors.add(
         attribute,
         I18n.t("activemodel.errors.models.#{record.model_name.singular}.domain_invalid")
