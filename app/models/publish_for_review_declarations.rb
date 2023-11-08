4732293a032f1bdb309b6465f95333964d09b6a9
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-11-08 16:12:16 +0000

    Update error handling

--- app/models/publish_for_review_declarations.rb
+++ app/models/publish_for_review_declarations.rb
@@ -3,7 +3,7 @@ class PublishForReviewDeclarations
 
   attr_accessor :declaration_1, :declaration_2, :declaration_3, :declaration_4, :declaration_5, :declaration_6, :declarations_checkboxes
 
-  validates :declaration_1, :declaration_2, :declaration_3, :declaration_4, :declaration_5, :declaration_6, presence: true
+  validate :all_declarations_selected
 
   def checked(params)
     @declaration_1 = params.include?('declaration_1')
@@ -12,5 +12,16 @@ class PublishForReviewDeclarations
     @declaration_4 = params.include?('declaration_4')
     @declaration_5 = params.include?('declaration_5')
     @declaration_6 = params.include?('declaration_6')
+  end
+
+  def all_declarations_selected
+    unless @declaration_1.present? &&
+        @declaration_2.present? &&
+        @declaration_3.present? &&
+        @declaration_4.present? &&
+        @declaration_5.present? &&
+        @declaration_6.present?
+      errors.add(:declarations_checkboxes, I18n.t('publish.declarations.error'))
+    end
   end
 end
