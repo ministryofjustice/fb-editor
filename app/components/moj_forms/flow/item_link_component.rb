0e5a552fa06e87b507faab874df60b1db86f8506
Chris Pymm <chris@chrispymm.co.uk>
2023-11-23 09:46:56 +0000

    Update  flow item page titles

--- app/components/moj_forms/flow/item_link_component.rb
+++ app/components/moj_forms/flow/item_link_component.rb
@@ -1,19 +1,20 @@
 module MojForms
   module Flow
     class ItemLinkComponent < ViewComponent::Base
-      attr_reader :title, :url
+      attr_reader :title, :type, :url
 
-      def initialize(title:, url:)
+      def initialize(title:, type:, url:)
         super
         @title = title
+        @type = type
         @url = url
       end
 
       def call
         tag.h2 class: 'govuk-body' do
           link_to url, class: 'govuk-link flow-item__title' do
-            concat tag.span("#{t('actions.edit')}: ", class: 'govuk-visually-hidden')
             concat tag.span(title, class: 'text')
+            concat tag.span(', ' + t("pages.flow.page_types.#{type}"), class: 'govuk-visually-hidden')
           end
         end
       end
