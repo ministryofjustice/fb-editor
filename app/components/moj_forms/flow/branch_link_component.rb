5cd58ec0d457f4db5c2339356fb665bf5cd58a37
Chris Pymm <chris@chrispymm.co.uk>
2023-11-23 22:41:07 +0000

    Update branching conditions tabbong and focus

--- app/components/moj_forms/flow/branch_link_component.rb
+++ app/components/moj_forms/flow/branch_link_component.rb
@@ -13,7 +13,7 @@ module MojForms
         tag.h2 class: 'govuk-body' do
           link_to url, class: 'govuk-link flow-item__title' do
             concat(
-              '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 125" tabindex="-1" focusable="false">
+              '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 125" tabindex="-1" focusable="false" role="presentation" aria-hidden="true">
                 <polygon fill="" points="1,62.5 100,1 199,62.5 100,124" stroke="" stroke-width="2"/>
                 </svg>'.html_safe
             )
