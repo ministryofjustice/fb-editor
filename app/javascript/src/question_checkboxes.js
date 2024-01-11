3a86f651f250337356cece3d872848ffcf671fa4
Chris Pymm <chris@chrispymm.co.uk>
2024-01-11 09:33:33 +0000

    WIP

--- app/javascript/src/question_checkboxes.js
+++ app/javascript/src/question_checkboxes.js
@@ -12,10 +12,9 @@
  *
  **/
 
-
-const utilities = require('./utilities');
+const utilities = require("./utilities");
 const mergeObjects = utilities.mergeObjects;
-const Question = require('./question');
+const Question = require("./question");
 
 const SELECTOR_HINT = "fieldset > .govuk-hint";
 const SELECTOR_LABEL = "legend > :first-child";
@@ -23,57 +22,81 @@ const SELECTOR_ITEM = ".govuk-checkboxes__item";
 const SELECTOR_ITEM_HINT = ".govuk-hint";
 const SELECTOR_ITEM_LABEL = "label";
 
-
 class CheckboxesQuestion extends Question {
   constructor($node, config) {
-    super($node, mergeObjects({
-      // Add stuff here if you want to set defaults
-      selectorLabel: SELECTOR_LABEL,
-      selectorHint: SELECTOR_HINT,
-      selectorCollectionItem: SELECTOR_ITEM,
-      selectorComponentCollectionItemLabel: SELECTOR_ITEM_LABEL,
-      selectorComponentCollectionItemHint: SELECTOR_ITEM_HINT,
+    super(
+      $node,
+      mergeObjects(
+        {
+          // Add stuff here if you want to set defaults
+          selectorLabel: SELECTOR_LABEL,
+          selectorHint: SELECTOR_HINT,
+          selectorCollectionItem: SELECTOR_ITEM,
+          selectorComponentCollectionItemLabel: SELECTOR_ITEM_LABEL,
+          selectorComponentCollectionItemHint: SELECTOR_ITEM_HINT,
 
-      filters: {
-        _id: function(index) {
-          return this.replace(/^(.*)?[\d]+$/, "$1" + index);
+          filters: {
+            _id: function (index) {
+              return this.replace(/^(.*)?[\d]+$/, "$1" + index);
+            },
+            value: function (index) {
+              return this.replace(/^(.*)?[\d]+$/, "$1" + index);
+            },
+          },
+
+          onCollectionItemClone: function ($node) {
+            // @node is the collection item (e.g. <div> wrapping <input type=radio> and <label> elements)
+            // Runs after the collection item has been cloned, so further custom manipulation can be
+            // carried out on the element.
+            $node.find("label").text(config.text.option);
+            $node.find("span").text(config.text.optionHint);
+          },
+          onItemRemove: function (item) {
+            // @item (EditableComponentItem) Item to be deleted.
+            // Runs before removing an editable Collection item.
+            // Provides an opportunity for clean up.
+            var activatedMenu = item.$node.data("ActivatedMenu");
+            if (activatedMenu) {
+              activatedMenu.activator.$node.remove();
+              activatedMenu.$node.remove();
+              activatedMenu.container.$node.remove();
+            }
+          },
+          onItemRemoveConfirmation: function (item) {
+            // @item (EditableComponentItem) Item to be deleted.
+            // Runs before onItemRemove when removing an editable Collection item.
+          },
         },
-        value: function(index) {
-          return this.replace(/^(.*)?[\d]+$/, "$1" + index);
-        }
-      },
-
-      onCollectionItemClone: function($node) {
-         // @node is the collection item (e.g. <div> wrapping <input type=radio> and <label> elements)
-         // Runs after the collection item has been cloned, so further custom manipulation can be
-         // carried out on the element.
-         $node.find("label").text(config.text.option);
-         $node.find("span").text(config.text.optionHint);
-      },
-      onItemRemove: function(item) {
-        // @item (EditableComponentItem) Item to be deleted.
-        // Runs before removing an editable Collection item.
-        // Provides an opportunity for clean up.
-        var activatedMenu = item.$node.data("ActivatedMenu");
-        if(activatedMenu) {
-          activatedMenu.activator.$node.remove();
-          activatedMenu.$node.remove();
-          activatedMenu.container.$node.remove();
-        }
-      },
-      onItemRemoveConfirmation: function(item) {
-        // @item (EditableComponentItem) Item to be deleted.
-        // Runs before onItemRemove when removing an editable Collection item.
-      }
-    }, config));
+        config,
+      ),
+    );
 
     $node.addClass("CheckboxesQuestion");
 
-    // If any Collection items are present with ability to be removed, we need
-    // to find them and scoop up the Remove buttons to put in menu component.
-    $node.find(".EditableComponentCollectionItem").first().parent().attr("aria-label", config.text.aria.answers);
+    $node.find("fieldset > legend").attr("aria-label", "Question");
+
+    $node
+      .find(".EditableComponentCollectionItem")
+      .first()
+      .parent()
+      .attr("aria-label", config.text.aria.answers);
 
     this._preservedItemCount = 1;
+
+    $node.find(".govuk-checkboxes__label").each(function (index) {
+      $(this).attr("aria-label", `Label for checkbox option ${index + 1}`);
+    });
+
+    $node.find(".govuk-checkboxes__hint").each(function (index) {
+      $(this).attr(
+        "aria-label",
+        `Optional hint text for checkbox option ${index + 1}`,
+      );
+    });
+
+    $node
+      .find(".govuk-checkboxes__hint")
+      .attr("aria-describedby", "optional_content_description");
   }
 }
 
