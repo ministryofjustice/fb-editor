0974b7f9f8947613bfe7dd656c76af8405f89e29
Chris Pymm <chris@chrispymm.co.uk>
2024-01-22 16:05:51 +0000

    Fix js tests

--- test/editable_components/editable_collection_field_component/callbacks_test.js
+++ test/editable_components/editable_collection_field_component/callbacks_test.js
@@ -1,48 +1,56 @@
+require("../../setup");
 
-require('../../setup');
-
-describe('EditableCollectionFieldComponment', function() {
-  const helpers = require('./helpers');
+describe("EditableCollectionFieldComponment", function () {
+  const helpers = require("./helpers");
   const c = helpers.constants;
-  const COMPONENT_ID = 'editable-collection-field-component-callbacks-test';
+  const COMPONENT_ID = "editable-collection-field-component-callbacks-test";
 
-  describe('Callbacks', function() {
+  describe("Callbacks", function () {
     var sandbox;
 
-    beforeEach(function() {
+    beforeEach(function () {
       sandbox = sinon.createSandbox();
     });
 
-    afterEach(function() {
+    afterEach(function () {
       sandbox.restore();
       helpers.teardownView(COMPONENT_ID);
     });
 
-    it('calls the onCollectionItemClone() callback', function() {
+    it("calls the onCollectionItemClone() callback", function () {
       var spy = sinon.spy();
-      var created = helpers.createEditableCollectionFieldComponent(COMPONENT_ID, {
-        onCollectionItemClone: spy,
-      });
+      var created = helpers.createEditableCollectionFieldComponent(
+        COMPONENT_ID,
+        {
+          onCollectionItemClone: spy,
+        },
+      );
 
-      expect(spy).to.have.been.calledOnce
+      expect(spy).to.have.been.calledOnce;
     });
 
-    it('calls the onItemAdd() callback', function() {
+    it("calls the onItemAdd() callback", function () {
       var spy = sinon.spy();
-      var created = helpers.createEditableCollectionFieldComponent(COMPONENT_ID, {
-        onItemAdd: spy,
-      });
+      var created = helpers.createEditableCollectionFieldComponent(
+        COMPONENT_ID,
+        {
+          onItemAdd: spy,
+        },
+      );
 
       created.instance.addItem();
 
       expect(spy).to.have.been.calledOnce;
     });
 
-    it('calls the onItemRemove() callback', function(){
+    it("calls the beforeItemRemove() callback", function () {
       var spy = sinon.spy();
-      var created = helpers.createEditableCollectionFieldComponent(COMPONENT_ID, {
-        onItemRemove: spy,
-      });
+      var created = helpers.createEditableCollectionFieldComponent(
+        COMPONENT_ID,
+        {
+          beforeItemRemove: spy,
+        },
+      );
 
       created.instance.removeItem(created.instance.items[1]);
 
