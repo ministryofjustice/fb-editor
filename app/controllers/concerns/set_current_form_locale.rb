3874bbbca70b6cf9cc43f83db505cc9bad409eb6
Jesus Laiz <zheileman@users.noreply.github.com>
2024-02-05 13:13:25 +0000

    Localise buttons when editing pages
    In the pages controller we switch locale according to the current form locale so we can localise things like the buttons (continue, save for later) that are coming from the metadata presenter.
    
    However to avoid duplicating all strings in the en.yml to cy.yml, we enable i18n.fallbacks so any locale not found in welsh, we use the english version as fallback.

--- app/controllers/concerns/set_current_form_locale.rb
+++ app/controllers/concerns/set_current_form_locale.rb
@@ -6,4 +6,10 @@ module SetCurrentFormLocale
       Current.form_locale = service.metadata.locale
     end
   end
+
+  private
+
+  def switch_locale(&action)
+    I18n.with_locale(Current.form_locale, &action)
+  end
 end
