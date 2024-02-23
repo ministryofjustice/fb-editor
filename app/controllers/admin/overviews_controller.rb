b131c7fc0139352c22eefd914db135009998539a
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2024-02-23 09:04:05 +0000

    is it the parentheses?

--- app/controllers/admin/overviews_controller.rb
+++ app/controllers/admin/overviews_controller.rb
@@ -68,7 +68,7 @@ module Admin
 
         format.csv do
           csv_data = CSV.generate do |csv|
-            csv << ['Service id', 'Service name', 'Confirmation email enabled', 'Save and return enabled', 'Collect data via email', 'Send to JSON api', 'Receive csv', 'External start page enabled', 'Start pages', 'Confirmation pages', 'Check your answers pages', 'Standalone pages', 'Exit pages', 'Single Question pages', 'Multiple Question pages', 'Address components', 'Autocomplete components', 'Checkbox components', 'Content components', 'Date components', 'Email components', 'Upload (old) components', 'Multiupload components', 'Number components', 'Radio components', 'Text input components', 'Textarea components']
+            csv << ['Service id', 'Service name', 'Confirmation email enabled', 'Save and return enabled', 'Collect data via email', 'Send to JSON api', 'Receive csv', 'External start page enabled', 'Start pages', 'Confirmation pages', 'Check your answers pages', 'Standalone pages', 'Exit pages', 'Single Question pages', 'Multiple Question pages', 'Address components', 'Autocomplete components', 'Checkbox components', 'Content components', 'Date components', 'Email components', 'Upload components', 'Multiupload components', 'Number components', 'Radio components', 'Text input components', 'Textarea components']
             summary.each do |s|
               stuff = to_csv_value(s)
               Rails.logger.info(stuff)
