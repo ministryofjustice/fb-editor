7a7be7a882bccf990d6edc3f11605256c6d65373
StevenLeighton21 <steven.leighton@digital.justice.gov.uk>
2024-02-27 08:53:58 +0000

    Export form feature summary as CSV (#2561)

--- app/controllers/admin/overviews_controller.rb
+++ app/controllers/admin/overviews_controller.rb
@@ -62,7 +62,51 @@ module Admin
       end
     end
 
+    def export_dev_form_summary
+      respond_to do |format|
+        summary = service_summary('dev')
+
+        format.csv do
+          csv_data = CSV.generate do |csv|
+            csv << service_summary_headers
+            summary.each do |s|
+              csv << to_csv_value(s)
+            end
+          end
+
+          send_data csv_data, filename: summary_csv_filename('test'), type: 'text/csv'
+        end
+      end
+    end
+
+    def export_live_form_summary
+      respond_to do |format|
+        summary = service_summary('production')
+
+        format.csv do
+          csv_data = CSV.generate do |csv|
+            csv << service_summary_headers
+            summary.each do |s|
+              csv << to_csv_value(s)
+            end
+          end
+
+          send_data csv_data, filename: summary_csv_filename('live'), type: 'text/csv'
+        end
+      end
+    end
+
     private
+
+    def to_csv_value(summary)
+      result = []
+
+      # rubocop:disable Style/ConditionalAssignment
+      summary.each_value { |v| v.is_a?(Hash) ? result << v.values.map!(&:to_s) : result << v.to_s }
+      # rubocop:enable Style/ConditionalAssignment
+
+      result.flatten
+    end
 
     def active_sessions
       cutoff_period = 90.minutes.ago
@@ -90,6 +134,103 @@ module Admin
       end
     end
 
+    def service_summary(env)
+      published_service_ids = published(env).reject { |p| moj_forms_team_service_ids.include?(p.service_id) }.map(&:service_id)
+      result = []
+
+      published_service_ids.each do |id|
+        metadata = MetadataApiClient::Service.latest_version(id)
+        features = ServiceConfiguration.where(service_id: id, deployment_environment: env).pluck(:name)
+
+        result << {
+          service_id: id,
+          service_name: metadata['service_name'],
+          locale: metadata['locale'],
+          page_counts: page_type_counts(metadata['pages']),
+          component_counts: component_type_counts(metadata['pages']),
+          confirmation_email_enabled: features.include?('CONFIRMATION_EMAIL_COMPONENT_ID'),
+          save_and_return_enabled: features.include?('SAVE_AND_RETURN'),
+          collect_information_by_email_enabled: features.include?('SERVICE_EMAIL_BODY'),
+          send_to_json_api_enabled: features.include?('SERVICE_OUTPUT_JSON_ENDPOINT'),
+          receive_csv_enabled: features.include?('SERVICE_CSV_OUTPUT'),
+          external_start_page_enabled: features.include?('EXTERNAL_START_PAGE_URL'),
+          analytics_enabled: features.include?('GA4'),
+          reference_number_enabled: features.include?('REFERENCE_NUMBER'),
+          payment_link_enabled: features.include?('PAYMENT_LINK')
+        }
+      end
+
+      result
+    end
+
+    def page_type_counts(pages)
+      types = []
+
+      pages.each do |page|
+        types << page['_type']
+      end
+
+      counts = types.tally
+
+      {
+        start_pages: counts.fetch('page.start', 0),
+        confirmation_pages: counts.fetch('page.confirmation', 0),
+        checkanswers_pages: counts.fetch('page.checkanswers', 0),
+        singlequestion_pages: counts.fetch('page.singlequestion', 0),
+        multiplequestions_pages: counts.fetch('page.multiplequestions', 0),
+        exit_pages: counts.fetch('page.exit', 0),
+        standalone_pages: counts.fetch('page.standalone', 0)
+      }
+    end
+
+    def component_type_counts(pages)
+      addresses = 0
+      autocompletes = 0
+      checkboxes = 0
+      contents = 0
+      dates = 0
+      emails = 0
+      uploads = 0
+      multiuploads = 0
+      numbers = 0
+      radios = 0
+      texts = 0
+      textareas = 0
+
+      pages.each do |page|
+        next if page['components'].blank?
+
+        types = []
+
+        page['components'].each do |component|
+          types << component['_type']
+        end
+
+        counts = types.tally
+
+        addresses += counts.fetch('address', 0)
+        autocompletes += counts.fetch('autocomplete', 0)
+        checkboxes += counts.fetch('checkboxes', 0)
+        contents += counts.fetch('content', 0)
+        dates += counts.fetch('date', 0)
+        emails += counts.fetch('email', 0)
+        uploads += counts.fetch('upload', 0)
+        multiuploads += counts.fetch('multiupload', 0)
+        numbers += counts.fetch('number', 0)
+        radios += counts.fetch('radios', 0)
+        texts += counts.fetch('text', 0)
+        textareas += counts.fetch('textarea', 0)
+      end
+
+      {
+        addresses:, autocompletes:, checkboxes:, contents:, dates:, emails:, uploads:, multiuploads:, numbers:, radios:, texts:, textareas:
+      }
+    end
+
+    def service_summary_headers
+      ['Service id', 'Service name', 'Locale', 'Start pages', 'Confirmation pages', 'Check your answers pages', 'Single Question pages', 'Multiple Question pages', 'Exit pages', 'Standalone pages', 'Address components', 'Autocomplete components', 'Checkbox components', 'Content components', 'Date components', 'Email components', 'Upload components', 'Multiupload components', 'Number components', 'Radio components', 'Text input components', 'Textarea components', 'Confirmation email enabled', 'Save and return enabled', 'Collect data via email', 'Send to JSON api', 'Receive csv', 'External start page enabled', 'Analytics enabled', 'Reference number enabled', 'Payment link enabled']
+    end
+
     def published_state(service_id, environment)
       currently_published?(service_id, environment) ? 'Yes' : 'No'
     end
@@ -110,6 +251,10 @@ module Admin
 
     def csv_filename
       "#{ENV['PLATFORM_ENV']}-services-#{Time.zone.now.strftime('%Y-%m-%d')}.csv"
+    end
+
+    def summary_csv_filename(humanised_env)
+      "#{humanised_env}-services-feature-summary-#{Time.zone.now.strftime('%Y-%m-%d')}.csv"
     end
 
     def moj_forms_team_service_ids
