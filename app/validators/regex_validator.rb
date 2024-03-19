472153cf022084a4f1312f68528025aded276631
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2024-03-19 18:10:02 +0000

    Adding some validations warnings for regex input in Editor

--- app/validators/regex_validator.rb
+++ app/validators/regex_validator.rb
@@ -1,5 +1,22 @@
 class RegexValidator < ActiveModel::Validator
+  DELIMITER = '/'.freeze
+  OPTIONS = %w[i x m o].freeze
+
   def validate(record)
+    if record.value.first == DELIMITER || record.value.last == DELIMITER
+      record.errors.add(
+        :pattern,
+        I18n.t('activemodel.errors.models.string_regex_pattern.remove_delimiter')
+      )
+    end
+
+    if OPTIONS.include?(record.value.split('/').last)
+      record.errors.add(
+        :pattern,
+        I18n.t('activemodel.errors.models.string_regex_pattern.remove_options')
+      )
+    end
+
     Regexp.new(record.value)
   rescue RegexpError
     record.errors.add(
