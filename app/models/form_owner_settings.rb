65baca4f630e764981d13fb32b5b43422883a8d9
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2024-04-11 18:59:02 +0100

    Returning the user email rather than the created_by field of the metadata

--- app/models/form_owner_settings.rb
+++ app/models/form_owner_settings.rb
@@ -5,12 +5,16 @@ class FormOwnerSettings
 
   def initialize(service_id:)
     @service_id = service_id
-    @form_owner = get_form_owner
+    @form_owner = get_form_owner_email
   end
 
-  def get_form_owner
+  private
+
+  def get_form_owner_email
     latest_metadata = MetadataApiClient::Service.latest_version(@service_id)
     latest_version = MetadataApiClient::Version.create(service_id: @service_id, payload: latest_metadata)
-    latest_version.created_by
+    owner_id = latest_version.created_by
+    owner_email = User.where(id: owner_id).pick(:email)
+    EncryptionService.new.decrypt(owner_email)
   end
 end
