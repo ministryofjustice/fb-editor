03a7ceb1f281a6b7881271d1e68d4b8dc61befaa
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2024-04-19 10:56:54 +0100

    Transfer ownership setting  (#2615)
    * Adding route, view and controller for transfer ownership
    
    * Adding view for transfer ownership
    
    * Adding model for form ownership transfer setting
    
    * Adding call to metadata api to get the field created by with form author
    
    * Returning the user email rather than the created_by field of the metadata
    
    * Adding route and controller to update form owner value
    
    * Adding display of error messages
    
    * Fix check if email is present in User DB
    
    * Refactor validation of user email
    
    * Updating version metadata with the new form owner
    
    * Content and user design updates
    
    * Updating tests for metadata api client to return the field created_by
    
    * Adding unit tests for transfer form ownership
    
    * Using mock factorybot user present for rspec
    
    * Adding modal for confirmation of transfer of ownership
    
    Co-authored-by: Chris Pymm <chris.pymm@digital.justice.gov.uk>
    
    * Fix brakeman security warning
    
    * Adding feature flags
    
    ---------
    
    Co-authored-by: Chris Pymm <chris.pymm@digital.justice.gov.uk>

--- config/routes.rb
+++ config/routes.rb
@@ -72,6 +72,8 @@ Rails.application.routes.draw do
         get '/form_owner', to: 'form_owner#index', as: :form_ownership
         put '/form_owner', to: 'form_owner#update', as: :transfer_form_ownership
         resources :ms_list, only: [:index, :create]
+        get '/form_owner', to: 'form_owner#index', as: :form_ownership
+        put '/form_owner', to: 'form_owner#update', as: :transfer_form_ownership
       end
 
       mount MetadataPresenter::Engine => '/preview', as: :preview
