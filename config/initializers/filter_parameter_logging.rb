823908ed15c278e606eedda6593a785307019851
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2024-04-22 17:18:36 +0100

    Adding those variables to the list of filtered parameter to not show in sentry

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,5 +4,5 @@
 # sensitive information. See the ActiveSupport::ParameterFilter documentation for supported
 # notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :email, :name, :username, :password, :authenticity_token
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :email, :name, :username, :password, :authenticity_token, :access_key, :secret_access_key
 ]
