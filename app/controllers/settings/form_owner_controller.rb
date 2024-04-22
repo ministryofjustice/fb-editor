7fbe5341c90d440942afa4fbc0d4d166f52114c4
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2024-04-22 13:43:20 +0100

    Adding information to ownership transfer confirmation email

--- app/controllers/settings/form_owner_controller.rb
+++ app/controllers/settings/form_owner_controller.rb
@@ -4,10 +4,11 @@ class Settings::FormOwnerController < FormController
   def index; end
 
   def update
+    previous_owner = User.find(service.created_by).email
     new_owner = params[:form_owner_settings][:form_owner]
     @form_owner = FormOwnerSettings.new(service_id: service.service_id, metadata: service.to_h, form_owner: new_owner)
     if @form_owner.update
-      send_confirmation_email(new_owner, @service.service_name)
+      send_confirmation_email(new_owner, @service.service_name, previous_owner)
       transfer_params = {
         service: @service.service_name,
         owner: new_owner
@@ -25,11 +26,14 @@ class Settings::FormOwnerController < FormController
     @form_owner = FormOwnerSettings.new
   end
 
-  def send_confirmation_email(new_owner, service_name)
+  def send_confirmation_email(new_owner, service_name, previous_owner)
     EmailService::Emailer.send_mail(
       to: new_owner,
-      subject: "Transfer of Ownership for #{service_name}",
-      body: 'Sample Email',
+      subject: 'An MoJ Forms form has been transferred to you',
+      body: "#{service_name}\n The following form has been transferred to you: \n#{service_name}\nYou are now able
+to access, edit and publish this form. It will be listed on the ‘Your forms’ page when you next sign in to MoJ Forms.\n
+Sign in to MoJ Forms\nThis change was requested by #{previous_owner}.\n
+If you have any questions, please contact us.",
       html: '<h1>Sample Email</h1>',
       raw: 'It\'s complicated'
     )
