bbc1b539973b0456f61fd14f9442ea331030651a
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2024-04-23 13:53:47 +0100

    Putting messages in locales

--- app/controllers/settings/form_owner_controller.rb
+++ app/controllers/settings/form_owner_controller.rb
@@ -28,14 +28,17 @@ class Settings::FormOwnerController < FormController
 
   def send_confirmation_email(new_owner, service_name, previous_owner)
     emailer = EmailService::Emailer.new
+
+    body = I18n.t('activemodel.errors.models.transfer_ownership.confirmation_email.subject',
+                  service_name:,
+                  previous_owner:,
+                  href: I18n.t('activemodel.errors.models.transfer_ownership.href'))
+
     emailer.send_mail(
       to: new_owner,
-      subject: 'An MoJ Forms form has been transferred to you',
-      body: "#{service_name}\n The following form has been transferred to you: \n#{service_name}\nYou are now able
-to access, edit and publish this form. It will be listed on the ‘Your forms’ page when you next sign in to MoJ Forms.\n
-Sign in to MoJ Forms\nThis change was requested by #{previous_owner}.\n
-If you have any questions, please contact us.",
-      html: '<h1>Sample Email</h1>'
+      subject: I18n.t('activemodel.errors.models.transfer_ownership.confirmation_email.body'),
+      body:,
+      html: body
     )
   end
 end
