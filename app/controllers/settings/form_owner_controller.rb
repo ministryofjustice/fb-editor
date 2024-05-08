81e29e16bb72c4a65d2c8cd1d32d5606e60dd0a9
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2024-05-08 17:01:29 +0100

    Transfer ownership confirmation email (#2627)
    * Adding step to send email and first test email
    
    * Tweaking message to have the right fields
    
    * Adding log trace and setting encoding to utf8
    
    * Adding forms information strings to the email
    
    * Removing the rescue error as it is not available as such in aws ses v2
    
    * Tweaking payload to ses
    
    * Adding credentials parameters to ses initialisation
    
    * Refactoring
    
    * Adding initialisation of ses client
    
    * Adding information to ownership transfer confirmation email
    
    * Adding initialisation of emailer
    
    * Adding those variables to the list of filtered parameter to not show in sentry
    
    * removing raw message
    
    * Putting messages in locales
    
    * using ses v3 sdk
    
    * Fixing locales not showing in controller
    
    * Moving sending transfer ownership email to another class
    
    * Tweaking content of transfer ownership email
    
    * Adding class for confirmation email
    
    * Using send raw email service
    
    * Adding links to the email
    
    * Linting
    
    * Using gov uk notify gem
    
    * Linting
    
    * Adding template reference for notify in configuration
    
    * Adding corresponding tests
    
    * Adding template for Test Notify service
    
    * WIP ses unit tests
    
    * Removing implementation of transfer ownership confirmation email using amazon ses
    
    * Tweaking view after mob testing feedback
    
    * Sending a email copy to previous form owner
    
    * Addressing comments from technical review
    
    * Adding case insensitive for email check in user DB
    
    * Adding another description for transfer form ownership in the settings
    
    * Linting
    
    * Adding error message just above the field
    
    * Adding the little red bar near the field
    
    * Adding unit tests for insensitive case match email
    
    * Adding another template for confirmation transfer of ownership to previous owner
    
    * Adding tests for transfer ownership email cc
    
    * Adding template reference for live
    
    * Adding unit tests for difference error messages
    
    * Linting

--- app/controllers/settings/form_owner_controller.rb
+++ app/controllers/settings/form_owner_controller.rb
@@ -4,11 +4,21 @@ class Settings::FormOwnerController < FormController
   def index; end
 
   def update
-    @form_owner = FormOwnerSettings.new(service_id: service.service_id, metadata: service.to_h, form_owner: params[:form_owner_settings][:form_owner])
+    previous_owner = User.find(service.created_by).email
+    new_owner = params[:form_owner_settings][:form_owner]
+    @form_owner = FormOwnerSettings.new(service_id: service.service_id, metadata: service.to_h, form_owner: new_owner)
     if @form_owner.update
+      TransferOwnershipMailer.transfer_ownership(
+        service_name: @service.service_name, previous_owner:, new_owner:
+      ).deliver_later
+
+      TransferOwnershipMailer.transfer_ownership_confirmation(
+        service_name: @service.service_name, previous_owner:, new_owner:
+      ).deliver_later
+
       transfer_params = {
         service: @service.service_name,
-        owner: params[:form_owner_settings][:form_owner]
+        owner: new_owner
       }
       redirect_to services_path(params: transfer_params)
     else
