83f0d8462eea8187797d28a27866319272806f42
Jesus Laiz <zheileman@users.noreply.github.com>
2024-05-15 11:40:12 +0100

    Refactor to simplify code
    `ACCEPTANCE_TESTS_USER` is not exposed in the pods, where we need it.
    
    At this point is easier to just use the hardcoded email address, and also refactor to make it simpler to add more email recipients to skip in the future.

--- app/lib/notify_mailer_interceptor.rb
+++ app/lib/notify_mailer_interceptor.rb
@@ -3,21 +3,21 @@ class NotifyMailerInterceptor
     # Skips Notify mail deliveries when running acceptance tests
     # Can be extended if needed in the future to cover other scenarios
 
+    SKIP_RECIPIENTS = %w[
+      fb-acceptance-tests@digital.justice.gov.uk
+    ].freeze
+
     def delivering_email(message)
-      if acceptance_tests?(message)
-        message.perform_deliveries = false
-        Rails.logger.info "Interceptor prevented sending email to: #{acceptance_test_email}"
-      end
+      return if allowed_recipient?(message.to.first)
+
+      message.perform_deliveries = false
+      Rails.logger.info "Interceptor prevented sending email to: #{message.to}"
     end
 
     private
 
-    def acceptance_tests?(message)
-      message.to.include?(acceptance_test_email)
-    end
-
-    def acceptance_test_email
-      ENV['ACCEPTANCE_TESTS_USER']
+    def allowed_recipient?(email)
+      SKIP_RECIPIENTS.exclude?(email)
     end
   end
 end
--- spec/lib/notify_mailer_interceptor_spec.rb
+++ spec/lib/notify_mailer_interceptor_spec.rb
@@ -4,17 +4,16 @@ describe NotifyMailerInterceptor do
   let(:message) do
     Mail::Message.new(
       from: ['from@example.com'],
-      to: ['to@example.com']
+      to: [recipient_email]
     )
   end
 
   before do
-    allow(ENV).to receive(:[]).with('ACCEPTANCE_TESTS_USER').and_return(tests_user_email)
     described_class.delivering_email(message)
   end
 
-  context 'when the email recipient matches the acceptance tests user' do
-    let(:tests_user_email) { 'to@example.com' }
+  context 'when the email recipient is the acceptance tests user' do
+    let(:recipient_email) { 'fb-acceptance-tests@digital.justice.gov.uk' }
 
     it 'does not perform deliveries' do
       expect(message.perform_deliveries).to be(false)
@@ -22,7 +21,7 @@ describe NotifyMailerInterceptor do
   end
 
   context 'when the email recipient is not the acceptance tests user' do
-    let(:tests_user_email) { 'john@example.com' }
+    let(:recipient_email) { 'john@example.com' }
 
     it 'performs deliveries' do
       expect(message.perform_deliveries).to be(true)
