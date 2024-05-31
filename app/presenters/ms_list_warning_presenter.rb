0de8494e0049d5bf7102de9a4c04cdcc61168826
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2024-05-31 11:46:35 +0100

    Add new files

---
+++ app/presenters/ms_list_warning_presenter.rb
@@ -0,0 +1,53 @@
+class MsListWarningPresenter
+  attr_accessor :service, :deployment_environment
+
+  def initialize(service:, deployment_environment:)
+    @deployment_environment = deployment_environment
+    @service = service
+  end
+
+  def message
+    # byebug
+    @publishes = if deployment_environment == 'dev'
+                   publishes_dev
+                 else
+                   publishes_production
+                 end
+    # byebug
+    return if first_publish?
+    return if latest.nil?
+
+    if latest.service.version_id == service.version_id
+      'need to create a new list for your changes'
+    end
+  end
+
+  def latest
+    @publishes&.last
+  end
+
+  def published?
+    latest&.published?
+  end
+
+  def first_publish?
+    last_unpublish = @publishes.unpublished.last
+    if last_unpublish
+      @publishes.published.since(last_unpublish.created_at).count == 1
+    else
+      @publishes.published.count == 1
+    end
+  end
+
+  def publishes_dev
+    @publishes_dev ||= PublishService.where(
+      service_id: service.service_id
+    ).dev
+  end
+
+  def publishes_production
+    @publishes_production ||= PublishService.where(
+      service_id: service.service_id
+    ).production
+  end
+end
