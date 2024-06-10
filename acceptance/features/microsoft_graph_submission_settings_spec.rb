6fcf560eb9386a18389bc0a440455463811dae3c
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2024-06-10 16:18:21 +0100

    Update spec, show drive only when configured

--- acceptance/features/microsoft_graph_submission_settings_spec.rb
+++ acceptance/features/microsoft_graph_submission_settings_spec.rb
@@ -14,6 +14,8 @@ feature 'Submission email' do
     scenario 'loading the settings page' do
       when_I_visit_the_ms_graph_submission_settings_page
       then_I_should_see_the_ms_graph_submission_settings_page(environment)
+      when_I_click_save(environment)
+      then_I_should_see_validation
     end
   end
 
@@ -45,5 +47,17 @@ feature 'Submission email' do
     within( "#ms-list-setting-#{environment}") do
       expect(page).to have_button(I18n.t("actions.saved"))
     end
+  end
+
+  def when_I_click_save(environment)
+    if environment == 'dev'
+      click_button(I18n.t('settings.submission.dev.save_button'))
+    else
+      click_button(I18n.t('settings.submission.production.save_button'))
+    end
+  end
+
+  def then_I_should_see_validation
+    expect(page).to have_content("Your answer for 'Ms site' cannot be blank.")
   end
 end
