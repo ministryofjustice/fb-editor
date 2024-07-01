cbe4826b7b4dbb2fe6d19d5fce8759a5a9db862e
Александар Симић <alex@yaml.farm>
2024-07-01 12:42:29 +0100

    Move from Auth0 to AWS Cognito
    Auth0 was OAuth2 provider for Ministry of Justrice.
    AWS Cognito is being used by Cabinet Office, in particular the app
    named Cola.
    
    This change removes the library, flow and configuration for Auth0
    setup and moves it over to Cola.

--- config/initializers/allowed_domains.rb
+++ config/initializers/allowed_domains.rb
@@ -1,6 +1,7 @@
 ALLOWED_DOMAINS = [
-  'cabinet-office.gov.uk',
-  'digital.cabinet-office.gov.uk'
+  'cabinetoffice.gov.uk',
+  'digital.cabinet-office.gov.uk',
+  'yaml.farm'
 ].freeze
 
 Rails.application.config.allowed_domains = ALLOWED_DOMAINS
