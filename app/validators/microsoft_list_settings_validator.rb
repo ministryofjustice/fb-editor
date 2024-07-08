ef78eeec3b3b4f80af777c6185ff7537d5603b8a
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2024-07-08 14:04:44 +0100

    Add new file

---
+++ app/validators/microsoft_list_settings_validator.rb
@@ -0,0 +1,18 @@
+class MicrosoftListSettingsValidator < ActiveModel::Validator
+  def validate(record)
+    site_id = record.ms_site_id
+    uuid_regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+
+    if site_id.blank?
+      record.errors.add(:ms_site_id, 'Enter a SharePoint site ID')
+      return false
+    end
+
+    unless site_id.match?(uuid_regex)
+      record.errors.add(:ms_site_id, 'Enter a valid SharePoint site ID')
+      return false
+    end
+
+    true
+  end
+end
