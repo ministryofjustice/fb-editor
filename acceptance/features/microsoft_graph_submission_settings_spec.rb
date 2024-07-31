fdd4b4eae66324f6d24ed87ecf71d990cc547f52
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2024-07-31 15:58:40 +0100

    Reintroduce a very basic spec

--- acceptance/features/microsoft_graph_submission_settings_spec.rb
+++ acceptance/features/microsoft_graph_submission_settings_spec.rb
@@ -12,12 +12,11 @@ feature 'Submission email' do
 
   shared_examples 'ms graph submission settings page' do
     scenario 'loading the settings page' do
-      # TODO: uncomment when releasing, for now this is only accessible to admin
-      # when_I_visit_the_ms_graph_submission_settings_page
-      # then_I_should_see_the_ms_graph_submission_settings_page(environment)
-      # when_I_enter_an_empty_id(environment)
-      # when_I_click_save(environment)
-      # then_I_should_see_validation
+      when_I_visit_the_ms_graph_submission_settings_page
+      then_I_should_see_the_ms_graph_submission_settings_page(environment)
+      when_I_enter_an_empty_id(environment)
+      when_I_click_save(environment)
+      then_I_should_see_validation
     end
   end
 
