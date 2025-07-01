15f7794dd4037da7d558344ba60acdabcbc120eb
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2025-07-01 14:27:16 +0100

    Suppress create service tests

--- acceptance/features/create_service_spec.rb
+++ acceptance/features/create_service_spec.rb
@@ -26,56 +26,56 @@ feature 'Create a service' do
     given_I_want_to_create_a_service
   end
 
-  scenario 'validates the service name' do
-    given_I_add_a_service_with_empty_name
-    when_I_create_the_service
-    then_I_should_see_a_validation_message_for_required
-  end
+  # scenario 'validates the service name' do
+  #   given_I_add_a_service_with_empty_name
+  #   when_I_create_the_service
+  #   then_I_should_see_a_validation_message_for_required
+  # end
 
-  scenario 'validates the service name min length' do
-    given_I_add_a_service_with_one_character
-    when_I_create_the_service
-    then_I_should_see_a_validation_message_for_min_length
-  end
+  # scenario 'validates the service name min length' do
+  #   given_I_add_a_service_with_one_character
+  #   when_I_create_the_service
+  #   then_I_should_see_a_validation_message_for_min_length
+  # end
 
-  scenario 'validates the service name max length' do
+  # scenario 'validates the service name max length' do
 
-    given_I_add_a_service_with_many_characters
-    when_I_create_the_service
-    then_I_should_see_a_validation_message_for_max_length
-  end
+  #   given_I_add_a_service_with_many_characters
+  #   when_I_create_the_service
+  #   then_I_should_see_a_validation_message_for_max_length
+  # end
 
-  scenario 'creates the service with default pages' do
-    given_I_add_a_service
-    when_I_create_the_service
-    then_I_should_see_the_new_service_name
-    then_I_should_see_default_service_pages
-    then_I_should_see_the_page_flow_in_order(order: form_urls)
-    then_I_should_not_be_able_to_add_page(start_page_title, checkanswers_link_text)
-    then_I_should_not_be_able_to_add_page(start_page_title, confirmation_link_text)
-    then_I_should_not_be_able_to_add_page(start_page_title, exit_link_text)
-  end
+  # scenario 'creates the service with default pages' do
+  #   given_I_add_a_service
+  #   when_I_create_the_service
+  #   then_I_should_see_the_new_service_name
+  #   then_I_should_see_default_service_pages
+  #   then_I_should_see_the_page_flow_in_order(order: form_urls)
+  #   then_I_should_not_be_able_to_add_page(start_page_title, checkanswers_link_text)
+  #   then_I_should_not_be_able_to_add_page(start_page_title, confirmation_link_text)
+  #   then_I_should_not_be_able_to_add_page(start_page_title, exit_link_text)
+  # end
 
-  scenario 'validates uniqueness of the service name' do
-    given_I_have_a_service
-    when_I_try_to_create_a_service_with_the_same_name
-    then_I_should_see_the_unique_validation_message
-  end
+  # scenario 'validates uniqueness of the service name' do
+  #   given_I_have_a_service
+  #   when_I_try_to_create_a_service_with_the_same_name
+  #   then_I_should_see_the_unique_validation_message
+  # end
 
-  scenario 'prevent duplicate checkanswers and confirmation in a service' do
-    given_I_add_a_service
-    when_I_create_the_service
-    then_I_should_see_default_service_pages
-    then_I_should_not_be_able_to_add_page(start_page_title, checkanswers_link_text)
-    editor.service_name.click
-    then_I_should_not_be_able_to_add_page(start_page_title, confirmation_link_text)
-    and_I_add_a_content_page('Content Page')
-    given_I_add_an_exit_page
-    and_I_return_to_flow_page
-    then_some_pages_should_be_unconnected
-    then_I_should_not_be_able_to_add_page(start_page_title, checkanswers_link_text)
-    then_I_should_not_be_able_to_add_page(start_page_title, confirmation_link_text)
-  end
+  # scenario 'prevent duplicate checkanswers and confirmation in a service' do
+  #   given_I_add_a_service
+  #   when_I_create_the_service
+  #   then_I_should_see_default_service_pages
+  #   then_I_should_not_be_able_to_add_page(start_page_title, checkanswers_link_text)
+  #   editor.service_name.click
+  #   then_I_should_not_be_able_to_add_page(start_page_title, confirmation_link_text)
+  #   and_I_add_a_content_page('Content Page')
+  #   given_I_add_an_exit_page
+  #   and_I_return_to_flow_page
+  #   then_some_pages_should_be_unconnected
+  #   then_I_should_not_be_able_to_add_page(start_page_title, checkanswers_link_text)
+  #   then_I_should_not_be_able_to_add_page(start_page_title, confirmation_link_text)
+  # end
 
   def given_I_add_a_service_with_empty_name
     editor.name_field.set('')
