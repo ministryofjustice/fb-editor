5c06978e6d704928c78c9e01ce400dc0b0c7555e
Mo Seedat <mohammed.seedat2@justice.gov.uk>
2025-10-31 00:39:01 +0000

    Move webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
-const path    = require("path")
-const webpack = require("webpack")
+// ORIGINAL WEBPACK CONFIG. Moved to config/webpack/webpack.config.js
+//
+// const path    = require("path")
+// const webpack = require("webpack")
 
-module.exports = {
-  mode: "production",
-  devtool: "source-map",
-  entry: {
-    application: "./app/javascript/application.js"
-  },
-  output: {
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-    chunkFormat: "module",
-    path: path.resolve(__dirname, "app/assets/builds"),
-  },
-  plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+// module.exports = {
+//   mode: "production",
+//   devtool: "source-map",
+//   entry: {
+//     application: "./app/javascript/application.js"
+//   },
+//   output: {
+//     filename: "[name].js",
+//     sourceMapFilename: "[file].map",
+//     chunkFormat: "module",
+//     path: path.resolve(__dirname, "app/assets/builds"),
+//   },
+//   plugins: [
+//     new webpack.optimize.LimitChunkCountPlugin({
+//       maxChunks: 1
+//     })
+//   ]
+// }
