edfc6093d67a9bbaeb75945dc5a5201101055167
Mo Seedat <mohammed.seedat2@justice.gov.uk>
2025-10-31 00:40:40 +0000

    Alter package.json and webpack.config.js paths

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     chunkFormat: "module",
-    path: path.resolve(__dirname, "app/assets/builds"),
+    path: path.resolve(__dirname, '..', '..', 'app/assets/builds')
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
