24f64fb1bb7b77fd128106c9564f3bd4a1ade308
matt tei <matt.tei@digital.justice.gov.uk>
2026-02-13 14:22:40 +0000

    Cleanup moj team and admins (#2877)
    * Added a new template id for notify for the first time email in test.
    
    * Remove users from admin and devs who are no longer in the team.

--- config/initializers/moj_forms_team.rb
+++ config/initializers/moj_forms_team.rb
@@ -1,15 +1,9 @@
 MOJ_FORMS_ADMIN = %w[
-  claire.bowman@digital.justice.gov.uk
-  sijy.mathew@digital.justice.gov.uk
   mark.jefferson@digital.justice.gov.uk
-  assma.banaris@digital.justice.gov.uk
-  fabien.marry@digital.justice.gov.uk
-  darren.rooke@digital.justice.gov.uk
   daniel.glen@digital.justice.gov.uk
   polly.parker@digital.justice.gov.uk
   andrew.altwasser@digital.justice.gov.uk
   deepika.patel@digital.justice.gov.uk
-  sijy.mathew1@justice.gov.uk
   Deepika.Patel@justice.gov.uk
   Andrew.Altwasser@justice.gov.uk
   Rhian.Townsend@justice.gov.uk
@@ -20,13 +14,6 @@ MOJ_FORMS_ADMIN = %w[
 MOJ_FORMS_DEVS = %w[
   matt.tei@digital.justice.gov.uk
   Matthew.Tei@Justice.gov.uk
-  chris.pymm@digital.justice.gov.uk
-  hellema.ibrahim@digital.justice.gov.uk
-  steven.leighton@digital.justice.gov.uk
-  steven.leighton1@justice.gov.uk
-  jesus.laiz@digital.justice.gov.uk
-  hettie.street@digital.justice.gov.uk
-  Hettie.Street@justice.gov.uk
   Nick.Preddy@Justice.gov.uk
   meena.modhvadia@digital.justice.gov.uk
   Meena.Modhvadia@justice.gov.uk
