d334e3c002c8aa009a29816f1489f124e8fd69bb
Ripan Kumar <er.ripankumar@gmail.com>
2026-06-01 10:17:14 +0100

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
-Rails.application.config.session_store :active_record_store, key: '_fb_editor_session'
+Rails.application.config.session_store :active_record_store,
+                                       key: '_fb_editor_session',
+                                       secure: Rails.env.production?
