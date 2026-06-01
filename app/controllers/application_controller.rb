d934d666c67a24aca24cceb0ec986bdc5421fefd
Ripan <ripan.kumar@justice.gov.uk>
2026-06-01 17:41:06 +0100

    CDPTKAN-779: MOJ forms host header poisoning (#2961)
    * Update application.rb
    
    * Add 'protect_from_forgery'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   include SetCurrentRequestDetails
   include Auth0Helper
   helper_method :current_user
