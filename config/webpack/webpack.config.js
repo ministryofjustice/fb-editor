5559d56ce9eeb1d42074f1feae579a91d5a3e6cf
lucas-shaw <129938801+lucas-shaw@users.noreply.github.com>
2026-08-14 12:08:58 +0100

    Update config/webpack/webpack.config.js

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,13 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
 const { generateWebpackConfig } = require('shakapacker')
 
-const webpackConfig = generateWebpackConfig()
+const webpackConfig = generateWebpackConfig({
+  resolve: {
+    alias: {
+      jquery: 'jquery/src/jquery',
+      'jquery-ui': 'jquery-ui-dist/jquery-ui.js',
+    },
+  },
+})
 
 module.exports = webpackConfig
